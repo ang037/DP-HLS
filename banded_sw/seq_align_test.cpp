cf056c1ff2db61da3c2c9aea0d3f3d99f3cc45f7
ang037 <ang037@AD.UCSD.EDU@raptor.ucsd.edu>
2023-02-07 19:10:56 -0800

    banded smith waterman algo for shorter sequences

---
+++ banded_sw/seq_align_test.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "seq_align.h"
+#include "params.h"
+#include <cstdlib>
+#include <iostream>
+#include "ap_int.h"
+#include "ap_fixed.h"
+
+using namespace std;
+
+int main ()
+{
+	//initializing query sequence
+	//FIXME - this can also be randomized
+    char query_string[query_length] = {'C', 'A', 'G', 'A' , 'C', 'C', 'T', 'A', 'C', 'A', 'G', 'A'};// , 'C', 'C', 'T', 'A', 'C', 'A', 'G', 'A' , 'C', 'C', 'T', 'A', 'C', 'A', 'G', 'A' , 'C', 'C', 'T', 'A'};
+
+    char reference_string[ref_length] = {'C', 'C', 'G', 'T', 'A', 'C', 'T', 'A', 'C', 'C', 'G', 'T'};//, 'A', 'C', 'T', 'A', 'C', 'C', 'G', 'T', 'A', 'C', 'T', 'A', 'C', 'C', 'G', 'T', 'A', 'C', 'T', 'A'};
+
+    //generating random reference sequence of given length specified in params.h
+    /*srand(time(NULL));
+
+    char reference_string[ref_length];
+
+    char alphabet[4] = { 'A', 'C', 'G','T'};
+ 
+    for (int i = 0; i < ref_length; i++){
+        reference_string[i] = alphabet[rand() % 4];
+    }*/
+
+    ap_int<16> dummy;
+
+    printf("Start of function call\n");
+
+    //calling sequence alignment function
+    seq_align(query_string, reference_string, &dummy);
+
+    return 0;
+
+}
