7053e69519f8d798c0558a028abe40c367815a6e
ioeddk <78573313+ioeddk@users.noreply.github.com>
2023-04-06 21:10:55 -0700

    setup branch Yingqi

---
+++ NOTE.sh
@@ -0,0 +1,5 @@
+# Note
+This file document the usages. 
+
+## util.sh
+This script is used to sync updated sourcefile into the vitis project. 
---
+++ vitis/README.md
@@ -0,0 +1,2 @@
+# Vitis
+This folder is used to store vitis projects, as well as the synthesized design. 
