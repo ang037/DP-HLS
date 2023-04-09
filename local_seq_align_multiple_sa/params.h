f6ab08e2ac240cda1281e236ac928d2da6895594
ang037 <ang037@AD.UCSD.EDU@raptor.ucsd.edu>
2023-04-09 14:40:30 -0700

    added multiple sa kernels

---
+++ local_seq_align_multiple_sa/params.h
@@ -0,0 +1,16 @@
+#define opening_score -1
+#define extend_score -1
+#define mismatch_score -1
+#define match_score 2
+
+#define query_length 64
+#define ref_length 64
+
+#define PE_num 32
+
+#define numofreads 1
+
+#define query_chunks query_length/PE_num
+
+#define M 10
+#define N 6
---
+++ local_seq_align_multiple_sa/seq_align_multiple.h
@@ -0,0 +1,21 @@
+//function prototype
+#include <gmp.h>
+#define __gmp_const const
+#include "params.h"
+#include "ap_int.h"
+#include "ap_fixed.h"
+#include "hls_stream.h"
+
+using namespace std;
+
+typedef ap_fixed<M,N> type_t;
+//typedef int type_t;
+//typedef ap_int<16> type_t;
+
+extern void seq_align_multiple(ap_uint<2> chunk1[query_length], ap_uint<2> chunk2[query_length], ap_uint<2> ref1[ref_length], ap_uint<2> ref2[ref_length],
+		type_t dp_mem[3][PE_num], type_t dp_mem2[3][PE_num],
+		type_t Ix_mem[2][PE_num], type_t Ix_mem2[2][PE_num],
+		type_t Iy_mem[2][PE_num],type_t Iy_mem2[2][PE_num],
+		type_t last_pe_score[ref_length], type_t last_pe_score2[ref_length],
+		type_t last_pe_scoreIx[ref_length], type_t last_pe_scoreIx2[ref_length],
+		type_t *dummy_3, type_t *dummy_4);
