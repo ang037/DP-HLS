c9a2a0acbae3034d807216bf3beaaa94402b2210
ioeddk <78573313+ioeddk@users.noreply.github.com>
2023-04-10 17:58:43 -0700

    temporarily unrolled using HLS inline
    Former-commit-id: 1bac28fc82335e3615b49690b34fa2820a1cac82 [formerly 6a8313a80412397074cbb8d5b36b35288b35a701]
    Former-commit-id: aa7755d6f41ade15404f5a1446126d302b42d040

--- src/local_seq_align/seq_align.h
+++
@@ -1,8 +0,0 @@
-//function prototype
-#include "params.h"
-
-extern void seq_align (char query[query_length], char reference[ref_length], int *dummy
-
-                        /*, int traceback[query_length][ref_length], 
-                        int Ix_matrix[query_length][ref_length],int Iy_matrix[query_length][ref_length], int final_matrix[query_length][ref_length]*/);
-
