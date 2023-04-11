1bac28fc82335e3615b49690b34fa2820a1cac82
ioeddk <78573313+ioeddk@users.noreply.github.com>
2023-04-10 17:58:43 -0700

    temporarily unrolled using HLS inline
    Former-commit-id: 6a8313a80412397074cbb8d5b36b35288b35a701

--- vitis_hls_init.sh
+++
@@ -1,4 +0,0 @@
-source /tools/Xilinx/Vitis/2022.2/settings64.sh
-source /opt/xilinx/xrt/setup.sh
-export PLATFORM_REPO_PATHS=/opt/xilinx/platforms/xilinx_u280_gen3x16_xdma_1_202211_1
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
