780d99a5eb4c4c889f947f4bddc5c8b46948eea9
ioeddk <78573313+ioeddk@users.noreply.github.com>
2023-04-13 16:46:20 -0700

    update file structure
    Former-commit-id: bd89ffd9bb4ce172e2a4f8230b9972b8be745269 [formerly d98131520df92a347e9b7599fcfbb5d5f64c9305]
    Former-commit-id: 9a1ebbb392747e9b3e723b22adf591c802acb4b8

--- local_seq_align_multiple_sa/seq_align_multiple.h
+++ local_seq_align_multiple_sa/seq_align_multiple.h
@@ -8,20 +8,23 @@
 
 using namespace std;
 
-typedef ap_fixed<M,N> type_t;
+typedef ap_fixed<M,N> type_t;  // alias type_t with ap_fixed<M,N>
 //typedef int type_t;
 //typedef ap_int<16> type_t;
 
-extern void seq_align_multiple(ap_uint<2> chunk1[query_length], ap_uint<2> chunk2[query_length], ap_uint<2> chunk3[query_length], ap_uint<2> chunk4[query_length],
-		ap_uint<2> chunk5[query_length], ap_uint<2> chunk6[query_length], ap_uint<2> chunk7[query_length], ap_uint<2> chunk8[query_length],
-		ap_uint<2> ref1[ref_length], ap_uint<2> ref2[ref_length], ap_uint<2> ref3[ref_length], ap_uint<2> ref4[ref_length],
-		ap_uint<2> ref5[ref_length], ap_uint<2> ref6[ref_length], ap_uint<2> ref7[ref_length], ap_uint<2> ref8[ref_length],
-		type_t *dummy1_out, type_t *dummy2_out, type_t *dummy3_out, type_t *dummy4_out,
-		type_t *dummy5_out, type_t *dummy6_out, type_t *dummy7_out, type_t *dummy8_out);
-																						   /*ap_uint<2> chunk1[query_length], ap_uint<2> chunk2[query_length], ap_uint<2> ref1[ref_length], ap_uint<2> ref2[ref_length],
-		type_t dp_mem[3][PE_num], type_t dp_mem2[3][PE_num],
-		type_t Ix_mem[2][PE_num], type_t Ix_mem2[2][PE_num],
-		type_t Iy_mem[2][PE_num],type_t Iy_mem2[2][PE_num],
-		type_t last_pe_score[ref_length], type_t last_pe_score2[ref_length],
-		type_t last_pe_scoreIx[ref_length], type_t last_pe_scoreIx2[ref_length],
-		type_t *dummy_3, type_t *dummy_4);*/
+//extern void seq_align_multiple(ap_uint<2> chunk1[query_length], ap_uint<2> chunk2[query_length], ap_uint<2> ref1[ref_length], ap_uint<2> ref2[ref_length],
+//		type_t dp_mem[3][PE_num], type_t dp_mem2[3][PE_num],
+//		type_t Ix_mem[2][PE_num], type_t Ix_mem2[2][PE_num],
+//		type_t Iy_mem[2][PE_num],type_t Iy_mem2[2][PE_num],
+//		type_t last_pe_score[ref_length], type_t last_pe_score2[ref_length],
+//		type_t last_pe_scoreIx[ref_length], type_t last_pe_scoreIx2[ref_length],
+//		type_t *dummy_3, type_t *dummy_4);
+
+extern void seq_align_multiple(ap_uint<2> query_string_comp[N_BLOCKS][query_length],
+                               ap_uint<2> reference_string_comp[N_BLOCKS][ref_length],
+                               type_t dp_mem[N_BLOCKS][3][PE_num],
+                               type_t Ix_mem[N_BLOCKS][2][PE_num],
+                               type_t Iy_mem[N_BLOCKS][2][PE_num],
+                               type_t last_pe_score[N_BLOCKS][ref_length],
+                               type_t last_pe_scoreIx[N_BLOCKS][ref_length],
+                               type_t *dummies);
