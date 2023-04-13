320f2a882f370469d145acf11f3ff48666b6b080
ioeddk <78573313+ioeddk@users.noreply.github.com>
2023-04-13 16:14:39 -0700

    update git ingore
    Former-commit-id: cb33daad1300319257f398ddd4842251d6c10f5b [formerly 7ebdae668e8deb135f186acc11d8e9e648d93584] [formerly baae3aeae0a59b791e6821b59960da46ecaf4d3c [formerly 81b7c11874522c82d9ee20cf1526de1ada989f90]]
    Former-commit-id: baae3aeae0a59b791e6821b59960da46ecaf4d3c
    Former-commit-id: 04ff698aa2fff670503029a207d36ccbd09aea37

--- local_seq_align_multiple_sa/local_seq_align_multiple_sa_vitis/local_seq_align_multiple_sa_vitis/script.tcl
+++ local_seq_align_multiple_sa/local_seq_align_multiple_sa_vitis/local_seq_align_multiple_sa_vitis/script.tcl
@@ -5,15 +5,15 @@
 ############################################################
 open_project local_seq_align_multiple_sa_vitis
 set_top seq_align_multiple
-add_files src/params.h
-add_files src/seq_align_multiple.cpp
 add_files src/seq_align_multiple.h
-add_files -tb src/params.h
-add_files -tb src/seq_align_test.cpp
+add_files src/seq_align_multiple.cpp
+add_files src/params.h
+add_files -tb src/seq_align_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb src/params.h -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "local_seq_align_multiple_sa_vitis" -flow_target vivado
 set_part {xcvu5p-flva2104-1-e}
 create_clock -period 10 -name default
-#source "./local_seq_align_multiple_sa_vitis/local_seq_align_multiple_sa_vitis/directives.tcl"
+source "./local_seq_align_multiple_sa_vitis/local_seq_align_multiple_sa_vitis/directives.tcl"
 csim_design
 csynth_design
 cosim_design
