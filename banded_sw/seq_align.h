0b83914c4afb7baaffd37be3ce2a7786a3466b0f
Anshu Gupta <114828525+ang037@users.noreply.github.com>
2023-04-15 09:08:22 -0700

    Delete banded_sw directory
    Former-commit-id: 89a70ea8c7ce408e262b3e7b20e10b274209e13a

--- banded_sw/seq_align.h
+++
@@ -1,8 +0,0 @@
-//function prototype
-#include "params.h"
-#include "ap_int.h"
-#include "ap_fixed.h"
-
-using namespace std;
-
-extern void seq_align (char query[query_length], char reference[ref_length], ap_int<16> *dummy);
