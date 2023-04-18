361c39e626c434ce5ec5fe61e9f3025e143f44b4
ang037 <ang037@AD.UCSD.EDU@raptor.ucsd.edu>
2023-04-17 18:22:19 -0700

    fixed cosim issue merging with HLS UNROLL changes
    Former-commit-id: 5602af947605c3b94db725fef37e0b5d38d612e7 [formerly a44cbd2a4e381a7fa84629fc561335a5bb2fcc00] [formerly 8a5b645cc389c2dd36aa978d895009b39b72377c [formerly d25c42715676a68f20c6f6b8d973874da6438e3c]]
    Former-commit-id: 8a5b645cc389c2dd36aa978d895009b39b72377c
    Former-commit-id: cc6e716b5e7b01b3fe30a6181ea8a06279d37093

--- local_seq_align_multiple_sa/seq_align_test.cpp
+++ local_seq_align_multiple_sa/seq_align_test.cpp
@@ -81,62 +81,11 @@ int main() {
         }
     }
 
-    // bad naming, dummy is used to hold the max_score in seq_align_multiple.cpp
-//    type_t dummy_3;
-//    type_t dummy_4;
-
     // dummy is used to hold the max_score in seq_align_multiple.cpp
 
     type_t dummies[N_BLOCKS];
 
-    type_t dp_mem[N_BLOCKS][3][PE_num];  // two of dp_mem, one for each block
-    type_t Iy_mem[N_BLOCKS][2][PE_num];
-    type_t Ix_mem[N_BLOCKS][2][PE_num];
-
-
-    type_t last_pe_score[N_BLOCKS][ref_length];
-    type_t last_pe_scoreIx[N_BLOCKS][ref_length];
-
-    // notice that the block iter loop could be merged for the initialization
-    local_dpmem_loop:
-    for (int block_i = 0; block_i < N_BLOCKS; block_i++){
-        for (int gg = 0; gg < 3; gg++) {
-            for (int ij = 0; ij < PE_num; ij++) {
-                dp_mem[block_i][gg][ij] = 0;
-            }
-        }
-    }
-
-    local_Ixmem_loop:
-    for (int block_i = 0; block_i < N_BLOCKS; block_i++){
-        for (int mm = 0; mm < 2; mm++) {
-            for (int nn = 0; nn < PE_num; nn++) {
-                Ix_mem[block_i][mm][nn] = 0;
-            }
-        }
-    }
-
-    local_Iymem_loop:
-    for (int block_i = 0; block_i < N_BLOCKS; block_i++){
-        for (int pp = 0; pp < 2; pp++) {
-            for (int rr = 0; rr < PE_num; rr++) {
-                Iy_mem[block_i][pp][rr] = 0;
-            }
-        }
-    }
-
-    for (int block_i = 0; block_i < N_BLOCKS; block_i++){
-        for (int ip = 0; ip < ref_length; ip++) {
-
-            last_pe_score[block_i][ip] = 0;
-            last_pe_scoreIx[block_i][ip] = 0;
-        }
-    }
-
-
-    seq_align_multiple(query_string_comp, reference_string_comp, dp_mem, Ix_mem, Iy_mem, last_pe_score, last_pe_scoreIx, (&dummies)[N_BLOCKS]);
-
-    //printf("max score is %d\n", dummy);
+    seq_align_multiple(query_string_comp, reference_string_comp, (&dummies)[N_BLOCKS]);
 
     return 0;
 
