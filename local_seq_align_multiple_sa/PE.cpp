e2ea767bced3f92f8d31897fd2928b6d93a245e8
ioeddk <78573313+ioeddk@users.noreply.github.com>
2023-04-25 19:07:32 -0700

    finished classes implementations
    Former-commit-id: b0adedf66770bf1284a9b4ee99b17141c58fed1c

--- local_seq_align_multiple_sa/PE.cpp
+++ local_seq_align_multiple_sa/PE.cpp
@@ -1,6 +1,5 @@
 #include "params.h"
 
-#include <ap_shift_reg.h>
 #include <hls_stream.h>
 #include <ap_int.h>
 #include <ap_fixed.h>
@@ -12,47 +11,34 @@
 PE::PE(void)
 {
     this->score = 0;
-    this->score_reg = nullptr;
-}
-
-PE::PE(ap_shift_reg<type_t, 2> score_reg)
-{
-    this->score_reg = &score_reg;
-}
-
-void PE::compute(void)
-{
-
-}
-
-void PE::update(void)
-{
-	
 }
 
 
-LinearPE::LinearPE(ap_shift_reg<type_t, 2> score_reg)
+void PE::compute(void){}
+
+void PE::update(void){}
+
+
+LinearPE::LinearPE(void)
 {
-    this->score_reg = &score_reg;
     this->score = 0;
 }
 
 void LinearPE::update()
 {
-    this->score_reg->shift(this->score);
+    this->score_reg.shift(this->score);
 }
 
 void LinearPE::compute(ap_uint<2> local_ref_val, ap_uint<2> local_query_val, 
-    type_t up_prev, type_t left_prev, type_t diag_prev,
-    ap_shift_reg<ap_fixed<M, N>, 3>& score,
+    ShiftRegister<type_t, 2> &up_score,
     type_t* final) {
 #pragma HLS inline
 
-    type_t d = opening_score + left_prev;
-    type_t i = opening_score + up_prev;
+    type_t d = opening_score + this->score_reg[0];
+    type_t i = opening_score + up_score[0];
     type_t temp_pe = 0;
 
-    type_t match = (local_query_val == local_ref_val) ? diag_prev + match_score : diag_prev + mismatch_score;
+    type_t match = (local_query_val == local_ref_val) ? up_score[1] + match_score : up_score[1] + mismatch_score;
 
     type_t max_value = (((d > i) ? d : i) > match) ? ((d > i) ? d : i) : match;
 
@@ -69,25 +55,16 @@ AffinePE::AffinePE(void)
     this->Iy = 0;
 }
 
-//AffinePE::AffinePE(ap_shift_reg<type_t, 2> &score_reg,
-//                        ap_shift_reg<type_t, 1> &Ix_reg,
-//                        ap_shift_reg<type_t, 1> &Iy_reg){
-//#pragma HLS inline
-//    this->score_reg = &score_reg;
-//    this->Ix_reg = &Ix_reg;
-//    this->Iy_reg = &Iy_reg;
-//}
-
 void AffinePE::compute(ap_uint<2> local_ref_val, ap_uint<2> local_query_val, 
-    ap_shift_reg<type_t, 2>& score_up,
-    ap_shift_reg<type_t, 1>& Ix_up,
+    ShiftRegister<type_t, 2>& score_up,
+    ShiftRegister<type_t, 1>& Ix_up,
     type_t* final) {
 #pragma HLS inline
 
-    const type_t a1 = this->score_reg.read(0) + opening_score;
-    const type_t a2 = this->Iy_reg.read(0) + extend_score;
-    const type_t a3 = score_up.read(0) + opening_score;
-    const type_t a4 = Ix_up.read(0) + extend_score;
+    const type_t a1 = this->score_reg[0] + opening_score;
+    const type_t a2 = this->Iy_reg[0] + extend_score;
+    const type_t a3 = score_up[0] + opening_score;
+    const type_t a4 = Ix_up[0] + extend_score;
 
 
     this->Iy = a1 > a2 ? a1 : a2;
@@ -96,7 +73,7 @@ void AffinePE::compute(ap_uint<2> local_ref_val, ap_uint<2> local_query_val,
 
     const type_t temp = 0;
 
-    const type_t match = (local_query_val == local_ref_val) ? score_up.read(1) + match_score : score_up.read(1) + mismatch_score;
+    const type_t match = (local_query_val == local_ref_val) ? score_up[1] + match_score : score_up[1] + mismatch_score;
 
     const type_t max_value = (((this->Iy > this->Ix) ? this->Iy : this->Ix) > match) ? ((this->Iy > this->Ix) ? this->Iy : this->Ix) : match;
 
--- local_seq_align_multiple_sa/seq_align_multiple.cpp
+++ local_seq_align_multiple_sa/seq_align_multiple.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <ap_int.h>
 #include "seq_align_multiple.h"
-#include <ap_shift_reg.h>
 #include <hls_stream.h>
 #include "PE.h"
 #include "seq_align.h"
@@ -23,8 +22,8 @@ using namespace hls;
  */
 extern "C" {
 
-	void seq_align_multiple(stream<ap_uint<2>, query_length> query_string_comp_blocks[N_BLOCKS],
-		stream<ap_uint<2>, ref_length> reference_string_comp_blocks[N_BLOCKS],
+	void seq_align_multiple(stream<ap_uint<2>, query_length> (&query_string_comp_blocks)[N_BLOCKS],
+		stream<ap_uint<2>, ref_length> (&reference_string_comp_blocks)[N_BLOCKS],
 		type_t dummies[N_BLOCKS]) {
 
 #pragma HLS INTERFACE axis port=query_string_comp_blocks
@@ -33,45 +32,24 @@ extern "C" {
 
 #pragma HLS array_partition variable=query_string_comp_blocks type=block factor=N_BLOCKS dim=1
 #pragma HLS array_partition variable=reference_string_comp_blocks type=block factor=N_BLOCKS dim=1
-//#pragma HLS array_partition variable=reference_string_comp type=block factor=N_BLOCKS
-//#pragma HLS array_partition variable=last_pe_score type=block factor=N_BLOCKS
-//#pragma HLS array_partition variable=last_pe_scoreIx type=block factor=N_BLOCKS
+
+
 
 		type_t dummies_inner[N_BLOCKS];  // actual alignment score, max of the dp matrix
 
+#pragma HLS array_partition variable=dummies type=complete
+#pragma HLS array_partition variable=dummies_inner type=complete
+
 		// create alignment group
 		SeqAlign align_group[N_BLOCKS];
-
-
-		static ap_shift_reg<ap_fixed<M, N>, 2> dp_mem[N_BLOCKS][PE_num];
-		static ap_shift_reg<type_t, 1> Ix_mem[N_BLOCKS][PE_num];
-		static ap_shift_reg<type_t, 1> Iy_mem[N_BLOCKS][PE_num];
-		stream<type_t, ref_length> last_pe_score[N_BLOCKS];
-		stream<type_t, ref_length> last_pe_scoreIx[N_BLOCKS];
-
-#pragma HLS array_partition variable=dp_mem type=block factor=N_BLOCKS
-#pragma HLS array_partition variable=Ix_mem type=block factor=N_BLOCKS
-#pragma HLS array_partition variable=Iy_mem type=block factor=N_BLOCKS
-
-		for (int block_i = 0; block_i < ref_length; block_i++) {
-			for (int i = 0; i < ref_length; i++) {
-				last_pe_score[block_i].write(0);
-				// last_pe_scoreIx[block_i].write(0);
-			}
-		}
+#pragma HLS array_partition variable=align_group dim=1 type=complete
 
 		// to be unrolled
 		align_expand:
 		for (int block_i = 0; block_i < N_BLOCKS; block_i++) {
-#pragma HLS UNROLL factor=8
-
+#pragma HLS unroll
 			align_group[block_i].align(query_string_comp_blocks[block_i],
 				reference_string_comp_blocks[block_i],
-				dp_mem[block_i],
-				Ix_mem[block_i],
-				Iy_mem[block_i],
-				last_pe_score[block_i],
-				last_pe_scoreIx[block_i],
 				dummies_inner[block_i]);
 		}
 
