952dd806c44f3cce7fc62e2ea2b36dbdb867d78e
ioeddk <78573313+ioeddk@users.noreply.github.com>
2023-04-30 21:40:45 -0700

    updated tbcontainer
    Former-commit-id: 496a7312add0c94229e7a8c4a858963c814655ca

--- local_seq_align_multiple_sa/PE.h
+++ local_seq_align_multiple_sa/PE.h
@@ -32,7 +32,7 @@ public:
 	void update();
 
 	void compute(ap_uint<2> local_ref_val, ap_uint<2> local_query_val,
-		ShiftRegister<type_t, 2> &up_score,
+		ShiftRegister<type_t, 2>& up_score,
 		type_t* final);
 
 private:
@@ -47,23 +47,36 @@ public:
 	ShiftRegister<type_t, 1> Ix_reg;
 	ShiftRegister<type_t, 1> Iy_reg;
 
+	type_t max_score;
+	ap_uint<16> max_row;
+	ap_uint<16> max_col;
+
 	AffinePE(void);
+
+	AffinePE(char PEIdx);
 
 	//AffinePE(ap_shift_reg<type_t, 2> &score_reg,
 	//ap_shift_reg<type_t, 1> &Ix_reg,
 	//ap_shift_reg<type_t, 1> &Iy_reg);
 
-	void update();
+	void update(hls::stream<tbp_t, ref_length* query_length / PE_num>& tbp_out);
 
 	void compute(ap_uint<2> local_ref_val, ap_uint<2> local_query_val,
 		ShiftRegister<type_t, 2>& score_up,
-		ShiftRegister<type_t, 1>& Ix_up,
-		type_t* final);
+		ShiftRegister<type_t, 1>& Ix_up);
+
+	void nextChunk();
 
 private:
 	type_t score;
 	type_t Ix;
 	type_t Iy;
+	tbp_t traceback_ptr;  // hold the traceback ptr of a compute call
+
+	char PEIdx;
+	ap_uint<16> curr_row;
+	ap_uint<16> curr_col;
+
 
 };
 #endif // !PE_H
