ea4be21aaca5439e27ea12391bdd4fc183c9632a
Yingqi Cao <yic033@ucsd.edu>
2023-05-14 16:39:07 -0700

    updated config.json
    Former-commit-id: 62987142c2c3a87722eb9a996d7e8f110c7fa13e [formerly fedfd4ccb9031a40ad03afe2bd9ae28b07cb0bf5] [formerly 257c6a8ce7f233b3f7e9c738ae9d6ec4afb9f496 [formerly 21275c5b13fcb11a7214d0ece5b0af566df477dc]]
    Former-commit-id: 257c6a8ce7f233b3f7e9c738ae9d6ec4afb9f496
    Former-commit-id: 316954e96fffc8d5b492f174aafae1b8edb790c8

--- dphls.py
+++ dphls.py
@@ -1,21 +1,22 @@
 import argparse
 import json
+import jinja2
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="dphls", 
+    parser = argparse.ArgumentParser(prog="dphls",
                                      description="Take config.json and output the project in the directory specified",
                                      epilog="Thanks for Using")
     parser.add_argument("-c", "--config", help="Specify the config file to use", type=str, required=True)
     args = parser.parse_args()
     config = None
-    with open(args.config)as config_file:
+    with open(args.config) as config_file:
         config = json.load(config_file)
-    
+
     check_configuration(config)
 
-    
     return 0
+
 
 def check_configuration(config):
     """
@@ -28,16 +29,14 @@ def check_configuration(config):
     if kernel['processing_element']['type'] not in valid_pe:
         raise NotImplementedError("Processing element type not implemented")
 
-
     if kernel['align_block']['type'] not in valid_align_block:
         raise NotImplementedError("Alignment type not implemented")
-
 
     if kernel['processing_element']['type'] == 'affine' and kernel['traceback']['pointer_bits'] == 2:
         raise Exception("Affine processing element cannot have 2 bit traceback pointer")
 
-
     return 0
+
 
 def write_source(config):
     """
@@ -46,6 +45,7 @@ def write_source(config):
 
     return 0
 
+
 def create_project(config):
     """
     create the vitis hls project
@@ -53,7 +53,6 @@ def create_project(config):
 
     return 0
 
+
 if __name__ == '__main__':
     main()
-
-
