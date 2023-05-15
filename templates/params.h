dbf120f327650ba5b17fcc6fa63dbaf6f643673a
Yingqi Cao <yic033@ucsd.edu>
2023-05-14 21:22:07 -0700

    write file in the pipeline
    Former-commit-id: b50a3869ab8d4e286590603e67857f6fabad3163

--- templates/params.h
+++ templates/params.h
@@ -3,7 +3,7 @@
 
 #include <ap_fixed.h>
 
-#define linear_gap_penalty {{ linear_gap_panelty }};
+#define linear_gap_penalty {{ linear_gap_penalty }};
 #define opening_score {{ opening_score }};
 #define extend_score {{ extend_score }};
 #define mismatch_score {{ mismatch_score }};
@@ -23,7 +23,7 @@
 
 #define extra_PE_num query_length%PE_num
 
-#define N_BLOCKS 8
+#define N_BLOCKS {{ num_blocks }};
 
 
 #define TB_PH 0  // this is place holder
@@ -34,13 +34,12 @@
 #define	TB_IY 5
 
 
-
 #define TB_LINE_SIZE 64  // This defines the length of a line of TB pointers. Must be larger than PE_num
 
 typedef ap_uint<2> char_t;
 typedef ap_fixed<M, N> type_t;  // alias type_t with ap_fixed<M,N>
 
-typedef ap_uint<{{ traceback_bits }}> tbp_t;
+typedef ap_uint< {{ traceback_bits }} > tbp_t;
 typedef ap_uint<3> tbp_line_t[TB_LINE_SIZE];
 
 #define zero_fp (type_t) 0
