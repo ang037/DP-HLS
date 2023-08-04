87ab147c76fe5911b77f7024743156c9f665be83
Yingqi Cao <yic033@ucsd.edu>
2023-08-03 22:41:49 -0700

    modularity checkpoint
    Former-commit-id: dc74b503716aa98f8711a6dcba28648495ea485c [formerly e81f22a7d22f90188443d07c0d3e978021b03e53]
    Former-commit-id: a7bde781e5404361cf909bd4a29fc9d7ef5d50e3

--- include/seq_align_multiple.h
+++ include/seq_align_multiple.h
@@ -1,12 +1,13 @@
 #ifndef SEQ_ALIGN_MULTIPLE_H
 #define SEQ_ALIGN_MULTIPLE_H
 
-
+#include <hls_task.h>
 #include "params.h"
 #include "ap_int.h"
 #include "ap_fixed.h"
 #include <hls_stream.h>
 #include "initial.h"
+#include <hls_streamofblocks.h>
 
 using namespace hls;
 
@@ -18,7 +19,7 @@ extern "C" {
 		stream<char_t, MAX_REFERENCE_LENGTH> (&reference_string_comp_blocks)[N_BLOCKS],
 		stream<hls::vector<type_t, N_LAYERS>, MAX_QUERY_LENGTH> (&init_qry_scr)[N_BLOCKS],
 		stream<hls::vector<type_t, N_LAYERS>, MAX_REFERENCE_LENGTH> (&init_ref_scr)[N_BLOCKS],
-		// int query_lengths, int reference_lengths,
+        int query_lengths, int reference_lengths,
 		stream<tbp_t, MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH> (&tb_streams)[N_BLOCKS]
 		);
 }
