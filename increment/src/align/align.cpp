06f29a8963751c67044bb7f1771e3119c379a99e
Yingqi Cao <yic033@ucsd.edu>
2023-08-19 20:40:30 -0700

    Applied traceback Pointers
    Former-commit-id: 418c8942a0469b1ee373202e2be6892346f7bae0

--- increment/src/align/align.cpp
+++ increment/src/align/align.cpp
@@ -73,206 +73,8 @@ void PrepareScores(
 }
 
 
-
-void AlignCLS::align(
-	hls::stream<char_t, MAX_QUERY_LENGTH> &query_stream,
-    hls::stream<char_t, MAX_REFERENCE_LENGTH> &reference_stream,
-    hls::stream<hls::vector<type_t, N_LAYERS>, MAX_QUERY_LENGTH> &init_qry_scr,
-    hls::stream<hls::vector<type_t, N_LAYERS>, MAX_REFERENCE_LENGTH> &init_ref_scr,
-	const int query_length, const int reference_length,
-    hls::stream<tbp_t, MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH> &traceback_out)
-{
-
-
-//#pragma HLS array_partition variable = staging type = complete
-//#pragma HLS array_partition variable = this->tbmat dim = 2 type = cyclic factor = 8
-//#pragma HLS array_partition variable = this->predicate dim = 1 type = complete
-//#pragma HLS array_partition variable = this->local_reference dim = 1 type = complete
-//#pragma HLS array_partition variable = this->local_query dim = 1 type = complete
-//#pragma HLS array_partition variable = this->last_pe_score type = complete
-//#pragma HLS array_partition variable = this->dp_mem type = complete
-//
-//#pragma HLS bind_storage variable = this->local_reference type = RAM_1WNR impl = LUTRAM
-//#pragma HLS bind_storage variable = this->local_query type = RAM_1WNR impl = LUTRAM
-//// #pragma HLS bind_storage variable = this->last_pe_score type = RAM_1WNR impl = LUTRAM
-//#pragma HLS bind_storage variable = this->dp_mem type = RAM_1WNR impl = LUTRAM
-
-	this->init(
-		query_stream,
-		reference_stream,
-		init_qry_scr,
-		init_ref_scr,
-		query_length,
-		reference_length,
-		traceback_out);
-
-	// iterating through the chunks of the larger dp matrix
-
-kernel:
-	for (int row_idx = 0; row_idx < query_length; row_idx += PE_NUM)
-	{
-		left_brim_init:
-		for (int i = 0; i < PE_NUM; i++)
-		{
-			this->staging[i] = this->init_staging_score[this->init_left_brim_addr++];
-			
-		} // initialize very first column of a chunk
-
-#ifdef DEBUG
-		this->debug->collect("initial staging", this->staging, PE_NUM); // ifdef debug
-#endif
-
-		this->compute_chunk(
-			PE_NUM < query_length - row_idx ? PE_NUM : query_length - row_idx,
-			reference_length,
-			row_idx);
-	}
-
-	this->tracer.traceback(
-		this->tbmat,
-		traceback_out,
-		TB_START_LEVEL,
-		TB_START_ROW,
-		TB_START_COL);
-}
-
-void AlignCLS::compute_chunk(const int active_pe, const int row_length, int tb_idx)
-{
-	char_t *reference_ptr = this->reference;
-	idx_t last_row_r = 0; // last row read
-	idx_t last_row_w = 0; // last row write
-
-	this->dp_mem.clear();
-
-	int pe_cnt[PE_NUM];
-#pragma HLS ARRAY_PARTITION variable = pe_cnt type = complete
-
-	init_pe_cnt:
-	for (int i = 0; i < PE_NUM; i++)
-	{
-#pragma HLS unroll
-		pe_cnt[i] = 0;
-	}
-
-	qry_populate:
-	for (int i = 0; i < PE_NUM; i++)
-	{
-		this->local_query.shift_left(
-			i < active_pe ? query[this->query_ptr++] : (char_t)0);
-	} // This is where we actually needs the left shift operations
-
-	wavefronts:
-	for (int i = 0; i < active_pe + row_length - 1; i++)
-	{
-#pragma HLS pipeline II = 1
-//#pragma HLS dependence variable = this->last_pe_score type = inter dependent = false
-//#pragma HLS dependence variable = this->PE_group type = intra dependent = false
-		this->dp_mem.shift_right(this->staging); // initialize the DP-Mem to be 0
-
-#ifdef DEBUG
-		this->debug->collect("staging", this->staging, PE_NUM); // ifdef debug
-#endif
-
-		if (i < active_pe)
-		{ // set the predicate
-			predicate.shift_right(true);
-		}
-		else if (i >= row_length)
-		{
-			predicate.shift_right(false);
-		}
-
-		this->local_reference.shift_right( // sihft the local reference
-			i < row_length ? *(reference_ptr++) : (char_t)0);
-
-		// PE Loop
-		this->PE_group[0].compute(
-			this->local_query[0],
-			this->local_reference[0],
-			this->last_pe_score[last_row_r],
-			this->dp_mem[0][0],
-			i == 0 ? this->zero_fp_arr : this->last_pe_score[last_row_r - 1], // diagnoal
-			this->staging[0],								// scores to write to the dp_mem
-			this->tbmat[tb_idx + 0][pe_cnt[0]],
-			predicate[0]);
-
-		if (predicate[0])
-			last_row_r++; // If read, update the pointer
-
-		for (int pi = 1; pi < PE_NUM; pi++)
-		{
-#pragma HLS unroll
-//#pragma HLS dependence variable = PE_group type = inter dependent = false
-			this->PE_group[pi].compute(
-				this->local_query[pi],
-				this->local_reference[pi],
-				dp_mem[pi - 1][0],
-				dp_mem[pi][0],
-				dp_mem[pi - 1][1],
-				this->staging[pi],
-				this->tbmat[tb_idx + pi][pe_cnt[pi]],
-				predicate[pi]);
-		}
-
-		for (int pi = 0; pi < PE_NUM; pi++)
-		{
-#pragma HLS unroll
-			if (predicate[pi])
-				pe_cnt[pi]++;
-		}
-
-#ifdef DEBUG
-		// this->debug->collect("staging", this->staging, PE_NUM);  // ifdef debug
-		// this->debug->collect("last_pe_score", this->last_pe_score, row_length);  // ifdef debug
-#endif
-		if (predicate[PE_NUM - 1])
-		{
-			this->last_pe_score[last_row_w++] = this->staging[PE_NUM - 1];
-		} // If write, update the pointer
-	}
-}
-
-void AlignCLS::init(
-	stream<char_t, MAX_QUERY_LENGTH> &query_stream,
-	stream<char_t, MAX_REFERENCE_LENGTH> &reference_stream,
-	stream<hls::vector<type_t, N_LAYERS>, MAX_QUERY_LENGTH> &init_qry_scr,
-	stream<hls::vector<type_t, N_LAYERS>, MAX_REFERENCE_LENGTH> &init_ref_scr,
-	const int query_length, const int reference_length,
-	stream<tbp_t, MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH> &traceback_out)
-{
-	reference_cpy:
-	for (int i = 0; i < MAX_REFERENCE_LENGTH; i++)
-	{
-		this->reference[i] = reference_stream.read();
-        hls::vector<type_t, N_LAYERS> tmp = init_ref_scr.read();
-		this->last_pe_score[i] = {tmp[0], tmp[1], tmp[2]};
-	}
-	query_cpy:
-	for (int i = 0; i < MAX_QUERY_LENGTH; i++)
-	{
-		this->query[i] = query_stream.read(); // copy query
-		this->init_staging_score[i] = init_qry_scr.read();
-	}
-
-	this->query_ptr = 0;
-
-#ifdef DEBUG
-	for (int i = 0; i < PE_NUM; i++)
-	{
-		this->PE_group[i].score = &this->debug->data.score[i];
-	}
-	for (int i = 0; i < MAX_QUERY_LENGTH; i++)
-	{
-		this->debug->data.query.push_back(this->query[i]);
-	}
-	for (int i = 0; i < MAX_REFERENCE_LENGTH; i++)
-	{
-		this->debug->data.ref.push_back(this->reference[i]);
-	}
-#endif // DEBUG
-}
-
 void Align::ChunkCompute(
+	idx_t chunk_row_offset, 
 	input_char_block_t &query,
 	char_t (&reference)[MAX_REFERENCE_LENGTH],
 	score_block_t &init_col_scr,
@@ -285,34 +87,43 @@ void Align::ChunkCompute(
 	bool predicate[PE_NUM];
 	Utils::Init::Predicate(predicate);
 
+	idx_t pe_col_offsets[PE_NUM];
+	Utils::Init::ArrSet<idx_t, PE_NUM>(pe_col_offsets, 0);
+
 	char_t local_query[PE_NUM];
 	char_t local_reference[PE_NUM];
 
-	stream_of_blocks<input_char_block_t> query_in_stm;
 	stream_of_blocks<input_char_block_t> reference_in_stm;
 
 	stream_of_blocks<dp_mem_block_t> dp_mem_stm;
-	stream_of_blocks<dp_mem_block_t> dp_mem_update_to_prepare;
 
 	stream_of_blocks<score_block_t> up_scores;
 	stream_of_blocks<score_block_t> diag_scores;
 	stream_of_blocks<score_block_t> left_scores;
+
+	stream_of_blocks<dp_mem_block_t> initialized;
+
 	stream_of_blocks<score_block_t> scores_out;
 	stream_of_blocks<tbp_block_t> tbp_out; 
 
-
-	Align::InitializeChunkColScore(init_col_scr, dp_mem_stm);
-	
-	Align::PrepareScores(dp_mem_stm, init_col_scr[0], (score_vec_t) 0, up_scores, diag_scores, left_scores);
+	// Initialize last PE scores pointer. 
+	idx_t last_pe_read_addr = 0;
+	score_vec_t last_pe_read_backup = (score_vec_t) 0;
 
 	for (int i = 0; i < query_length + reference_length - 1; i++){
+		// Shift Predicate
 		if (i < query_length) { Utils::Array::ShiftRight<bool, PE_NUM>(predicate, true); }
 		else if (i >= reference_length) { Utils::Array::ShiftRight<bool, PE_NUM>(predicate, false); };
 
+		// Shift Reference
 		if (i < reference_length) { Utils::Array::ShiftRight<char_t, PE_NUM>(local_reference, reference[i]); }
 		else {Utils::Array::ShiftRight<char_t, PE_NUM>(local_reference, 0);}
 
+		// Write Reference to Block
 		Utils::Array::WriteStreamBlock<char_t, PE_NUM>(local_reference, reference_in_stm);
+
+		Align::WriteInitialColScore(i, init_col_scr, dp_mem_stm, initialized);
+		Align::PrepareScores(initialized, init_row_scr[last_pe_read_addr], last_pe_read_backup, up_scores, diag_scores, left_scores);
 
 		PE::ExpandComputeBlock(
 			query,
@@ -324,29 +135,47 @@ void Align::ChunkCompute(
 			tbp_out
 		);
 
-		Align::DPMemUpdate(dp_mem_stm, scores_out, dp_mem_update_to_prepare);
-		Align::PrepareScores(dp_mem_update_to_prepare, init_col_scr[0], (score_vec_t) 0, up_scores, diag_scores, left_scores);
-		
+		Align::DPMemUpdate(initialized, scores_out, dp_mem_stm);
+		last_pe_read_backup = init_row_scr[last_pe_read_addr++];
+
+
+		ArrangeTBP(tbp_out, predicate, pe_col_offsets, chunk_tbp_out);
 	}
 }
 
+void Align::WriteInitialColScore(int i, score_vec_t (&init_scores)[PE_NUM], hls::stream_of_blocks<dp_mem_block_t> &dp_mem_in, hls::stream_of_blocks<dp_mem_block_t> &scores_out)
+{
+	read_lock<dp_mem_block_t> dp_mem_rd(dp_mem_in);
+	write_lock<dp_mem_block_t> scores_wr(scores_out);
 
+#pragma HLS array_partition variable = dp_mem_rd type = complete
+#pragma HLS array_partition variable = scores_wr type = complete
 
+	for (int j = 0; j < PE_NUM; j++){
+#pragma HLS unroll
+		scores_wr[0][j] = dp_mem_rd[0][j];
+		scores_wr[1][j] = dp_mem_rd[1][j];
+	}
 
-void align_wp(hls::stream<char_t, MAX_QUERY_LENGTH> &query_stream,
-              hls::stream<char_t, MAX_REFERENCE_LENGTH> &reference_stream,
-              hls::stream<hls::vector<type_t, N_LAYERS>, MAX_QUERY_LENGTH> &init_qry_scr,
-              hls::stream<hls::vector<type_t, N_LAYERS>, MAX_REFERENCE_LENGTH> &init_ref_scr,
-              int query_length, int reference_length,
-              hls::stream<tbp_t, MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH> &traceback_out){
-    AlignCLS align;
-    align.align(
-            query_stream,
-            reference_stream,
-            init_qry_scr,
-            init_ref_scr,
-            query_length, reference_length,
-            traceback_out
-            );
+	if (i < PE_NUM){
+		write_lock<dp_mem_block_t> scores_wr(scores_out);
+		scores_wr[0][i] = init_scores[i];
+	}
+}
+
+void Align::ArrangeTBP(hls::stream_of_blocks<tbp_block_t> &tbp_in, bool (&predicate)[PE_NUM], idx_t (&pe_offset)[PE_NUM], hls::stream_of_blocks<tbp_chunk_block_t> &tbp_chunk_out)
+{
+	read_lock<tbp_block_t> tbp_rd(tbp_in);
+	write_lock<tbp_chunk_block_t> tbp_chunk_wr(tbp_chunk_out);
+
+#pragma HLS array_partition variable = tbp_rd type = complete
+#pragma HLS array_partition variable = tbp_chunk_wr type = cyclic factor = PE_NUM dim = 1
+
+	for (int i = 0; i < PE_NUM; i++){
+#pragma HLS unroll
+		if (predicate[i]){
+			tbp_chunk_wr[i][pe_offset[i]++] = tbp_rd[i];
+		}
+	}
 
 }
--- src/align/align.cpp
+++ src/align/align.cpp
@@ -11,19 +11,18 @@
 
 using namespace hls;
 
-
 void Align::align(
-        BlockInputs inputs,
-        tbp_t (&traceback_out)[MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH]) {
+	BlockInputs inputs,
+	tbp_t (&traceback_out)[MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH])
+{
 
-
-/*
- * hls::stream<char_t, MAX_QUERY_LENGTH>&query_stream,
-            hls::stream<char_t, MAX_REFERENCE_LENGTH>&reference_stream,
-            hls::stream<hls::vector<type_t, N_LAYERS>, MAX_QUERY_LENGTH> &init_qry_scr,
-            hls::stream<hls::vector<type_t, N_LAYERS>, MAX_REFERENCE_LENGTH> &init_ref_scr,
-            int query_length, int reference_length,
- */
+	/*
+	 * hls::stream<char_t, MAX_QUERY_LENGTH>&query_stream,
+				hls::stream<char_t, MAX_REFERENCE_LENGTH>&reference_stream,
+				hls::stream<hls::vector<type_t, N_LAYERS>, MAX_QUERY_LENGTH> &init_qry_scr,
+				hls::stream<hls::vector<type_t, N_LAYERS>, MAX_REFERENCE_LENGTH> &init_ref_scr,
+				int query_length, int reference_length,
+	 */
 
 #pragma HLS array_partition variable = staging type = complete
 #pragma HLS array_partition variable = this->tbmat dim = 1 type = cyclic factor = PE_NUM
@@ -35,203 +34,210 @@ void Align::align(
 
 #pragma HLS bind_storage variable = this->tbmat type = RAM_1WNR impl = BRAM
 
-//#pragma HLS bind_storage variable = this->local_reference type = RAM_1WNR impl = LUTRAM
-//#pragma HLS bind_storage variable = this->local_query type = RAM_1WNR impl = LUTRAM
-//// #pragma HLS bind_storage variable = this->last_pe_score type = RAM_1WNR impl = LUTRAM
-//#pragma HLS bind_storage variable = this->dp_mem type = RAM_1WNR impl = LUTRAM
+	// #pragma HLS bind_storage variable = this->local_reference type = RAM_1WNR impl = LUTRAM
+	// #pragma HLS bind_storage variable = this->local_query type = RAM_1WNR impl = LUTRAM
+	//// #pragma HLS bind_storage variable = this->last_pe_score type = RAM_1WNR impl = LUTRAM
+	// #pragma HLS bind_storage variable = this->dp_mem type = RAM_1WNR impl = LUTRAM
 
+	int query_length = inputs.query_length;
+	int reference_length = inputs.reference_length;
+	this->query_ptr = 0;
 
-    int query_length = inputs.query_length;
-    int reference_length = inputs.reference_length;
-    this->query_ptr = 0;
+	// iterating through the chunks of the larger dp matrix
 
-    // iterating through the chunks of the larger dp matrix
+kernel:
+	for (int row_idx = 0; row_idx < query_length; row_idx += PE_NUM)
+	{
+	left_brim_init:
+		for (int i = 0; i < PE_NUM; i++)
+		{
+			this->staging[i] = this->init_staging_score[this->init_left_brim_addr++];
 
-    kernel:
-    for (int row_idx = 0; row_idx < query_length; row_idx += PE_NUM) {
-        left_brim_init:
-        for (int i = 0; i < PE_NUM; i++) {
-            this->staging[i] = this->init_staging_score[this->init_left_brim_addr++];
-
-        } // initialize very first column of a chunk
+		} // initialize very first column of a chunk
 
 #ifdef DEBUG
-        this->debug->collect("initial staging", this->staging, PE_NUM); // ifdef debug
+		this->debug->collect("initial staging", this->staging, PE_NUM); // ifdef debug
 #endif
 
-        this->compute_chunk(
-                PE_NUM < query_length - row_idx ? PE_NUM : query_length - row_idx,
-                reference_length,
-                row_idx);
-    }
+		this->compute_chunk(
+			PE_NUM < query_length - row_idx ? PE_NUM : query_length - row_idx,
+			reference_length,
+			row_idx);
+	}
 
-    this->tracer.traceback(
-            this->tbmat,
-            traceback_out,
-            TB_START_LEVEL,
-            TB_START_ROW,
-            TB_START_COL);
+	this->tracer.traceback(
+		this->tbmat,
+		traceback_out,
+		TB_START_LEVEL,
+		TB_START_ROW,
+		TB_START_COL);
 }
 
-void Align::compute_chunk(const int active_pe, const int row_length, int tb_idx) {
-    char_t *reference_ptr = this->reference;
-    idx_t last_row_r = 0; // last row read
-    idx_t last_row_w = 0; // last row write
+void Align::compute_chunk(const int active_pe, const int row_length, int tb_idx)
+{
+	char_t *reference_ptr = this->reference;
+	idx_t last_row_r = 0; // last row read
+	idx_t last_row_w = 0; // last row write
 
-    this->dp_mem.clear();
+	this->dp_mem.clear();
 
-    int pe_cnt[PE_NUM];
+	int pe_cnt[PE_NUM];
 #pragma HLS ARRAY_PARTITION variable = pe_cnt type = complete
 
-    init_pe_cnt:
-    for (int i = 0; i < PE_NUM; i++) {
+init_pe_cnt:
+	for (int i = 0; i < PE_NUM; i++)
+	{
 #pragma HLS unroll
-        pe_cnt[i] = 0;
-    }
+		pe_cnt[i] = 0;
+	}
 
-    qry_populate:
-    for (int i = 0; i < PE_NUM; i++) {
-        this->local_query.shift_left(
-                i < active_pe ? query[this->query_ptr++] : (char_t) 0);
-    } // This is where we actually needs the left shift operations
+qry_populate:
+	for (int i = 0; i < PE_NUM; i++)
+	{
+		this->local_query.shift_left(
+			i < active_pe ? query[this->query_ptr++] : (char_t)0);
+	} // This is where we actually needs the left shift operations
 
-    wavefronts:
-    for (int i = 0; i < active_pe + row_length - 1; i++) {
+wavefronts:
+	for (int i = 0; i < active_pe + row_length - 1; i++)
+	{
 #pragma HLS pipeline II = 1
-//#pragma HLS dependence variable = this->last_pe_score type = inter dependent = false
-//#pragma HLS dependence variable = this->PE_group type = intra dependent = false
-        this->dp_mem.shift_right(this->staging); // initialize the DP-Mem to be 0
+		// #pragma HLS dependence variable = this->last_pe_score type = inter dependent = false
+		// #pragma HLS dependence variable = this->PE_group type = intra dependent = false
+		this->dp_mem.shift_right(this->staging); // initialize the DP-Mem to be 0
 
 #ifdef DEBUG
-        this->debug->collect("staging", this->staging, PE_NUM); // ifdef debug
+		this->debug->collect("staging", this->staging, PE_NUM); // ifdef debug
 #endif
 
-        if (i < active_pe) { // set the predicate
-            predicate.shift_right(true);
-        } else if (i >= row_length) {
-            predicate.shift_right(false);
-        }
+		if (i < active_pe)
+		{ // set the predicate
+			predicate.shift_right(true);
+		}
+		else if (i >= row_length)
+		{
+			predicate.shift_right(false);
+		}
 
-        this->local_reference.shift_right( // sihft the local reference
-                i < row_length ? *(reference_ptr++) : (char_t) 0);
+		this->local_reference.shift_right( // sihft the local reference
+			i < row_length ? *(reference_ptr++) : (char_t)0);
 
-        // Process_PE_Expand:
-        // pe_wrap(
-        //         PE_group, this->local_query,
-        //         this->local_reference, this->last_pe_score, this->dp_mem, i, last_row_r,
-        //         tb_idx, pe_cnt, this->staging, this->tbmat, predicate
-        // );
+		// Process_PE_Expand:
+		// pe_wrap(
+		//         PE_group, this->local_query,
+		//         this->local_reference, this->last_pe_score, this->dp_mem, i, last_row_r,
+		//         tb_idx, pe_cnt, this->staging, this->tbmat, predicate
+		// );
 
-        Single_PE_0:
-PE_group[0].compute(
-        local_query[0],
-        local_reference[0],
-        last_pe_score[last_row_r],
-        dp_mem[0][0],
-        i == 0 ? zero_fp_arr : last_pe_score[last_row_r - 1], // diagnoal
-        staging[0],                                // scores to write to the dp_mem
-        tbmat[tb_idx + 0][pe_cnt[0]],
-        predicate[0]);
+	Single_PE_0:
+		PE_group[0].compute(
+			local_query[0],
+			local_reference[0],
+			last_pe_score[last_row_r],
+			dp_mem[0][0],
+			i == 0 ? zero_fp_arr : last_pe_score[last_row_r - 1], // diagnoal
+			staging[0],											  // scores to write to the dp_mem
+			tbmat[tb_idx + 0][pe_cnt[0]],
+			predicate[0]);
 
-if (predicate[0])
-        last_row_r++; // If read, update the pointer
+		if (predicate[0])
+			last_row_r++; // If read, update the pointer
 
-PE_Expand_Loop:
-for (int pi = 1; pi < PE_NUM; pi++) {
+	PE_Expand_Loop:
+		for (int pi = 1; pi < PE_NUM; pi++)
+		{
 #pragma HLS unroll
-//#pragma HLS dependence variable = PE_group type = inter dependent = false
-        PE_group[pi].compute(
-                local_query[pi],
-                local_reference[pi],
-                dp_mem[pi - 1][0],
-                dp_mem[pi][0],
-                dp_mem[pi - 1][1],
-                staging[pi],
-                tbmat[tb_idx + pi][pe_cnt[pi]],
-                predicate[pi]);
-}
+			// #pragma HLS dependence variable = PE_group type = inter dependent = false
+			PE_group[pi].compute(
+				local_query[pi],
+				local_reference[pi],
+				dp_mem[pi - 1][0],
+				dp_mem[pi][0],
+				dp_mem[pi - 1][1],
+				staging[pi],
+				tbmat[tb_idx + pi][pe_cnt[pi]],
+				predicate[pi]);
+		}
 
-        for (int pi = 0; pi < PE_NUM; pi++) {
+		for (int pi = 0; pi < PE_NUM; pi++)
+		{
 #pragma HLS unroll
-            if (predicate[pi])
-                pe_cnt[pi]++;
-        }
+			if (predicate[pi])
+				pe_cnt[pi]++;
+		}
 
 #ifdef DEBUG
-        // this->debug->collect("staging", this->staging, PE_NUM);  // ifdef debug
-        // this->debug->collect("last_pe_score", this->last_pe_score, row_length);  // ifdef debug
+		// this->debug->collect("staging", this->staging, PE_NUM);  // ifdef debug
+		// this->debug->collect("last_pe_score", this->last_pe_score, row_length);  // ifdef debug
 #endif
-        if (predicate[PE_NUM - 1]) {
-            this->last_pe_score[last_row_w++] = this->staging[PE_NUM - 1];
-        } // If write, update the pointer
-    }
+		if (predicate[PE_NUM - 1])
+		{
+			this->last_pe_score[last_row_w++] = this->staging[PE_NUM - 1];
+		} // If write, update the pointer
+	}
 }
-
 
 Align::Align(char_t (&query)[MAX_QUERY_LENGTH], char_t (&reference)[MAX_REFERENCE_LENGTH],
-             hls::vector<type_t, N_LAYERS>  (&init_staging_score)[MAX_QUERY_LENGTH],
-             hls::vector<type_t, N_LAYERS> (&last_pe_score)[MAX_REFERENCE_LENGTH]) :
-        query(query), reference(reference),
-        init_staging_score(init_staging_score), last_pe_score(last_pe_score) {}
-
+			 hls::vector<type_t, N_LAYERS> (&init_staging_score)[MAX_QUERY_LENGTH],
+			 hls::vector<type_t, N_LAYERS> (&last_pe_score)[MAX_REFERENCE_LENGTH]) : query(query), reference(reference),
+																					 init_staging_score(init_staging_score), last_pe_score(last_pe_score) {}
 
 void align_wp(hls::stream<BlockInputs> &inputs_stm,
-              hls::stream<BlockOutputs> &traceback_out) {
-    BlockInputs input = inputs_stm.read();
-    Align align(
-            input.query,
-            input.reference,
-            input.init_col_score,
-            input.init_row_score
-    );
-    BlockOutputs output;
-    align.align(
-            input,
-            output.traceback
-    );
-    traceback_out.write(output);
-
+			  hls::stream<BlockOutputs> &traceback_out)
+{
+	BlockInputs input = inputs_stm.read();
+	Align align(
+		input.query,
+		input.reference,
+		input.init_col_score,
+		input.init_row_score);
+	BlockOutputs output;
+	align.align(
+		input,
+		output.traceback);
+	traceback_out.write(output);
 }
 
 void pe_wrap(
-        PE (&array)[PE_NUM],
-        ShiftRegister<char_t, PE_NUM> &local_query,
-        ShiftRegister<char_t, PE_NUM> &local_reference,
-        hls::vector<type_t, N_LAYERS> (&last_pe_score)[MAX_REFERENCE_LENGTH],
-        ShiftRegisterBlock<hls::vector<type_t, N_LAYERS>, PE_NUM, 2> &dp_mem,
-        int &i, idx_t &last_row_r, int &tb_idx, int (&pe_cnt)[PE_NUM],
-        hls::vector<type_t, N_LAYERS>  (&staging)[PE_NUM],
-        hls::vector<tbp_t, N_LAYERS> (&tbmat)[MAX_QUERY_LENGTH][MAX_REFERENCE_LENGTH],
-        ShiftRegister<bool, PE_NUM> &predicate
-){
-        hls::vector<type_t, N_LAYERS>  zero_fp_arr = (type_t) 0;
+	PE (&array)[PE_NUM],
+	ShiftRegister<char_t, PE_NUM> &local_query,
+	ShiftRegister<char_t, PE_NUM> &local_reference,
+	hls::vector<type_t, N_LAYERS> (&last_pe_score)[MAX_REFERENCE_LENGTH],
+	ShiftRegisterBlock<hls::vector<type_t, N_LAYERS>, PE_NUM, 2> &dp_mem,
+	int &i, idx_t &last_row_r, int &tb_idx, int (&pe_cnt)[PE_NUM],
+	hls::vector<type_t, N_LAYERS> (&staging)[PE_NUM],
+	hls::vector<tbp_t, N_LAYERS> (&tbmat)[MAX_QUERY_LENGTH][MAX_REFERENCE_LENGTH],
+	ShiftRegister<bool, PE_NUM> &predicate)
+{
+	hls::vector<type_t, N_LAYERS> zero_fp_arr = (type_t)0;
 
 Single_PE_0:
-array[0].compute(
-        local_query[0],
-        local_reference[0],
-        last_pe_score[last_row_r],
-        dp_mem[0][0],
-        i == 0 ? zero_fp_arr : last_pe_score[last_row_r - 1], // diagnoal
-        staging[0],                                // scores to write to the dp_mem
-        tbmat[tb_idx + 0][pe_cnt[0]],
-        predicate[0]);
+	array[0].compute(
+		local_query[0],
+		local_reference[0],
+		last_pe_score[last_row_r],
+		dp_mem[0][0],
+		i == 0 ? zero_fp_arr : last_pe_score[last_row_r - 1], // diagnoal
+		staging[0],											  // scores to write to the dp_mem
+		tbmat[tb_idx + 0][pe_cnt[0]],
+		predicate[0]);
 
-if (predicate[0])
-        last_row_r++; // If read, update the pointer
+	if (predicate[0])
+		last_row_r++; // If read, update the pointer
 
 PE_Expand_Loop:
-for (int pi = 1; pi < PE_NUM; pi++) {
+	for (int pi = 1; pi < PE_NUM; pi++)
+	{
 #pragma HLS unroll
-//#pragma HLS dependence variable = PE_group type = inter dependent = false
-        array[pi].compute(
-                local_query[pi],
-                local_reference[pi],
-                dp_mem[pi - 1][0],
-                dp_mem[pi][0],
-                dp_mem[pi - 1][1],
-                staging[pi],
-                tbmat[tb_idx + pi][pe_cnt[pi]],
-                predicate[pi]);
-}
+		// #pragma HLS dependence variable = PE_group type = inter dependent = false
+		array[pi].compute(
+			local_query[pi],
+			local_reference[pi],
+			dp_mem[pi - 1][0],
+			dp_mem[pi][0],
+			dp_mem[pi - 1][1],
+			staging[pi],
+			tbmat[tb_idx + pi][pe_cnt[pi]],
+			predicate[pi]);
+	}
 }
