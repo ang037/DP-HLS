6d00b80b492ed383ffac1ae0a5816e501cf9eb50
Yingqi Cao <yic033@ucsd.edu>
2023-09-28 10:31:37 -0700

    Debugged double free after register Result class.
    Former-commit-id: eab37e652ca7fa7cdb3ed1f5ddcc5fcf72426ea6 [formerly 4c73890158d6c9e4d070dc864002bf45e5bc909c]
    Former-commit-id: a22a7cbc56c053fe56b6de99acfef0d335144e1d

--- demo.py
+++ demo.py
@@ -1,6 +1,17 @@
 import alignhls
+import tracemalloc
+
+# tracemalloc.start()
 
 query = "CCGTAGACCCGAACTTCGCGGTACACCTTCTGAAACCGTCCCTAATCCGACGAGCGCCTTGAGAACG"
 reference = "TGAGAACGTAGTCTAGGCGAATCGGCCCTTGTATATCGGGGCCGTAGACCCGAACTTCGCGGTACAC"
+result = alignhls.kernel_traceback_path()
 
-result = alignhls.kernel_traceback_path(query, reference)
+print()
+
+# snapshot = tracemalloc.take_snapshot()
+# top_stats = snapshot.statistics('lineno')
+
+# print("[ Top 10 ]")
+# for stat in top_stats[:10]:
+#     print(stat)
