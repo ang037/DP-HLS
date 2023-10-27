a88f54816af0df742cf101cbcab332549501a63b
Yingqi Cao <yic033@ucsd.edu>
2023-10-27 05:37:19 -0700

    Post-Implementation GlobalAffine
    Former-commit-id: 13e11ecd5d55d87d349f5a2b9c52b695dae04863

--- src/pyapi.cpp
+++ src/pyapi.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-void AHRunner::run()
+void AHRunner::run(py::dict py_penalties)
 {
     std::vector<char> query(this->query.begin(), this->query.end());
     std::vector<char> reference(this->reference.begin(), this->reference.end());
@@ -24,7 +24,6 @@ void AHRunner::run()
     char_t query_buff[N_BLOCKS][MAX_QUERY_LENGTH];
 
     idx_t qry_lengths[N_BLOCKS], ref_lengths[N_BLOCKS];
-
 
     // Fill query buffer and references buffer for all blocks.
     // Each buffer is of MAX size, but only the actual length
@@ -48,11 +47,12 @@ void AHRunner::run()
     }
 
     Penalties penalties;  // FIXME: Add his to Python interafce
-    penalties.match = 2;
-    penalties.mismatch = -2;
-    penalties.open = -1;
-    penalties.extend = -1;
-    
+    penalties.match = (type_t) py_penalties["match"].cast<float>();
+    penalties.mismatch = (type_t) py_penalties["mismatch"].cast<float>();
+    penalties.open = (type_t) py_penalties["open"].cast<float>();
+    penalties.extend = (type_t) py_penalties["extend"].cast<float>();
+    penalties.linear_gap = (type_t) py_penalties["linear_gap"].cast<float>();
+
 // Actual kernel calling
     seq_align_multiple_static(
         query_buff,
@@ -63,7 +63,59 @@ void AHRunner::run()
         this->tb_streams); 
 }
 
-void AHRunner::run(string query_string, string reference_string)
+void AHRunner::run(Penalties penalties)
+{
+    std::vector<char> query(this->query.begin(), this->query.end());
+    std::vector<char> reference(this->reference.begin(), this->reference.end());
+ 
+    try {
+        if (query.size() > MAX_QUERY_LENGTH) throw std::runtime_error("Query length should less than MAX_QUERY_LENGTH, "
+            "actual query len " + std::to_string(query.size()) + ", Allocated qry len: " + std::to_string(MAX_QUERY_LENGTH));
+        if (reference.size() > MAX_REFERENCE_LENGTH) throw std::runtime_error("Reference length should less than MAX_REFERENCE_LENGTH, "
+            "actual ref len " + std::to_string(reference.size()) + ", Allocated ref len: " + std::to_string(MAX_REFERENCE_LENGTH));
+    } catch (const std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        throw;
+    }
+
+    char_t reference_buff[N_BLOCKS][MAX_REFERENCE_LENGTH];
+    char_t query_buff[N_BLOCKS][MAX_QUERY_LENGTH];
+
+    idx_t qry_lengths[N_BLOCKS], ref_lengths[N_BLOCKS];
+
+    // Fill query buffer and references buffer for all blocks.
+    // Each buffer is of MAX size, but only the actual length
+    // elements is filled.
+    for (int b = 0; b < N_BLOCKS; b++)
+    {
+        for (int i = 0; i < query.size(); i++)
+        {
+            query_buff[b][i] = this->base_to_num(query[i]);
+        }
+        for (int i = 0; i < reference.size(); i++)
+        {
+            reference_buff[b][i] = this->base_to_num(reference[i]);
+        }
+    }
+
+    for (int b = 0; b < N_BLOCKS; b++)
+    {
+        qry_lengths[b] = query.size();
+        ref_lengths[b] = reference.size();
+    }
+
+// Actual kernel calling
+    seq_align_multiple_static(
+        query_buff,
+        reference_buff,
+        qry_lengths,
+        ref_lengths,
+        penalties,
+        this->tb_streams); 
+}
+
+
+void AHRunner::run(string query_string, string reference_string, py::dict py_penalties)
 {
 
     // , unordered_map<string, float> penalties
@@ -108,11 +160,12 @@ void AHRunner::run(string query_string, string reference_string)
         ref_lengths[b] = reference.size();
     }
 
-    Penalties penalties;  // FIXME: Add his to Python interafce
-    penalties.match = 2;
-    penalties.mismatch = -2;
-    penalties.open = -1;
-    penalties.extend = -1;
+    Penalties penalties;
+    penalties.match = (type_t) py_penalties["match"].cast<float>();
+    penalties.mismatch = (type_t) py_penalties["mismatch"].cast<float>();
+    penalties.open = (type_t) py_penalties["open"].cast<float>();
+    penalties.extend = (type_t) py_penalties["extend"].cast<float>();
+    penalties.linear_gap = (type_t) py_penalties["linear_gap"].cast<float>();
 
     seq_align_multiple_static(
         query_buff,
