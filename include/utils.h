89b0efc362f99ed57e49f2454ec684e395f4b2c6
Yingqi Cao <yic033@ucsd.edu>
2023-12-12 01:56:55 +0000

    Host and bitstream successfully compile
    Former-commit-id: d428488da5d5ad863795f69665daab12322df04c [formerly be2ded4248c025e0c182c844ebffe463ee3065bd] [formerly d60918293c569347e7fe7f9a005b701807d94c70 [formerly 33bb6a6ad1a1c98899c1cbe1b57df4210b35aa81]]
    Former-commit-id: d60918293c569347e7fe7f9a005b701807d94c70
    Former-commit-id: 9ae933edf95563ef33303fa94409f01b7c1e8b26

--- include/utils.h
+++ include/utils.h
@@ -9,20 +9,20 @@ namespace Utils
 
 	namespace Array
 	{
-		template <typename T, int N>
-		void WriteStreamBlock(T arr[N], hls::stream_of_blocks<T[N]> &stream_block)
-		{
-			hls::write_lock<T[N]> wr(stream_block);
+// 		template <typename T, int N>
+// 		void WriteStreamBlock(T arr[N], hls::stream_of_blocks<T[N]> &stream_block)
+// 		{
+// 			hls::write_lock<T[N]> wr(stream_block);
 
-#pragma HLS array_partition variable = arr type = complete
-#pragma HLS array_partition variable = wr type = complete
+// #pragma HLS array_partition variable = arr type = complete
+// #pragma HLS array_partition variable = wr type = complete
 
-			for (int i = 0; i < N; i++)
-			{
-#pragma HLS unroll
-				wr[i] = arr[i];
-			}
-		}
+// 			for (int i = 0; i < N; i++)
+// 			{
+// #pragma HLS unroll
+// 				wr[i] = arr[i];
+// 			}
+// 		}
 		template <typename T, int N>
 		void ShiftLeft(T (&arr)[N], T new_data)
 		{
@@ -135,76 +135,76 @@ namespace Utils
 	namespace Matrix
 	{
 
-		template <typename T, int M, int N>
-		void WriteStreamBlock(T arr[M][N], hls::stream_of_blocks<T[M][N]> &stream_block)
-		{
-			hls::write_lock<T[M][N]> wr(stream_block);
+// 		template <typename T, int M, int N>
+// 		void WriteStreamBlock(T arr[M][N], hls::stream_of_blocks<T[M][N]> &stream_block)
+// 		{
+// 			hls::write_lock<T[M][N]> wr(stream_block);
 
-			for (int i = 0; i < M; i++)
-			{
-#pragma HLS unroll
-				for (int j = 0; j < N; j++)
-				{
-#pragma HLS unroll
-					wr[i][j] = arr[i][j];
-				}
-			}
-		}
+// 			for (int i = 0; i < M; i++)
+// 			{
+// #pragma HLS unroll
+// 				for (int j = 0; j < N; j++)
+// 				{
+// #pragma HLS unroll
+// 					wr[i][j] = arr[i][j];
+// 				}
+// 			}
+// 		}
 
-		template <typename T, int M, int N>
-		void ReadStreamBlock(T (&arr)[M][N], hls::stream_of_blocks<T[M][N]> &stream_block)
-		{
-			hls::read_lock<T[M][N]> rd(stream_block);
+// 		template <typename T, int M, int N>
+// 		void ReadStreamBlock(T (&arr)[M][N], hls::stream_of_blocks<T[M][N]> &stream_block)
+// 		{
+// 			hls::read_lock<T[M][N]> rd(stream_block);
 
-			for (int i = 0; i < M; i++)
-			{
-#pragma HLS unroll
-				for (int j = 0; j < N; j++)
-				{
-#pragma HLS unroll
-					arr[i][j] = rd[i][j];
-				}
-			}
-		}
+// 			for (int i = 0; i < M; i++)
+// 			{
+// #pragma HLS unroll
+// 				for (int j = 0; j < N; j++)
+// 				{
+// #pragma HLS unroll
+// 					arr[i][j] = rd[i][j];
+// 				}
+// 			}
+// 		}
 
-		/**
-		 * @brief This function is for alias typname for steram of blocks
-		 *
-		 * @tparam T: For example, typedef int T[16]
-		 * @param arr: array form of redefined type
-		 * @param stream_block: stream of the block type
-		 */
-		template <typename T, int M, int N>
-		void WriteStreamBlock(T arr, hls::stream_of_blocks<T> &stream_block)
-		{
-			hls::write_lock<T> wr(stream_block);
+// 		/**
+// 		 * @brief This function is for alias typname for steram of blocks
+// 		 *
+// 		 * @tparam T: For example, typedef int T[16]
+// 		 * @param arr: array form of redefined type
+// 		 * @param stream_block: stream of the block type
+// 		 */
+// 		template <typename T, int M, int N>
+// 		void WriteStreamBlock(T arr, hls::stream_of_blocks<T> &stream_block)
+// 		{
+// 			hls::write_lock<T> wr(stream_block);
 
-			for (int i = 0; i < M; i++)
-			{
-#pragma HLS unroll
-				for (int j = 0; j < N; j++)
-				{
-#pragma HLS unroll
-					wr[i][j] = arr[i][j];
-				}
-			}
-		}
+// 			for (int i = 0; i < M; i++)
+// 			{
+// #pragma HLS unroll
+// 				for (int j = 0; j < N; j++)
+// 				{
+// #pragma HLS unroll
+// 					wr[i][j] = arr[i][j];
+// 				}
+// 			}
+// 		}
 
-		template <typename T, int M, int N>
-		void ReadStreamBlock(T &arr, hls::stream_of_blocks<T> &stream_block)
-		{
-			hls::read_lock<T> rd(stream_block);
+// 		template <typename T, int M, int N>
+// 		void ReadStreamBlock(T &arr, hls::stream_of_blocks<T> &stream_block)
+// 		{
+// 			hls::read_lock<T> rd(stream_block);
 
-			for (int i = 0; i < M; i++)
-			{
-#pragma HLS unroll
-				for (int j = 0; j < N; j++)
-				{
-#pragma HLS unroll
-					arr[i][j] = rd[i][j];
-				}
-			}
-		}
+// 			for (int i = 0; i < M; i++)
+// 			{
+// #pragma HLS unroll
+// 				for (int j = 0; j < N; j++)
+// 				{
+// #pragma HLS unroll
+// 					arr[i][j] = rd[i][j];
+// 				}
+// 			}
+// 		}
 
 	}
 
