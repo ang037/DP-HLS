3aeda1cbafdc78d9aa6bf4451a1800d834b7f178
Yingqi Cao <yic033@ucsd.edu>
2024-01-30 09:17:29 +0000

    Done new traceback. Tested multiple kernel instances.
    Former-commit-id: 56c53042d47c3fbfc273e2c55a3eb0a31fab3eee [formerly f4c1b21db9da078597b1b3cb30fba7e846ad88a1]
    Former-commit-id: d9a88ba2376ad2d31d4cd07178d76789012d47ac

--- src/toplevel/seq_align_multiple.cpp
+++ src/toplevel/seq_align_multiple.cpp
@@ -1,24 +1,20 @@
-#include <stdio.h>
-#include <math.h>
-#include "../../include/params.h"
-#include <cstdlib>
-#include <iostream>
-#include <ap_int.h>
-#include "../../include/seq_align_multiple.h"
-
 #include <hls_stream.h>
-#include "../../include/PE.h"
-#include "../../include/align.h"
-#include "../../include/initial.h"
-// #include <hls_np_channel.h>
+#include <ap_int.h>
+
+#include "params.h"
+#include "seq_align_multiple.h"
+#include "PE.h"
+#include "align.h"
+#include "initial.h"
 
 #ifdef CMAKEDEBUG
-#include "../../include/debug.h"
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <cstdlib>
+#include "debug.h"
 #endif
 
-// Link to vitis_hls/2022.2/include
-
-// using namespace std;
 using namespace hls;
 
 /*
@@ -27,33 +23,34 @@ using namespace hls;
 extern "C"
 {
 	void seq_align_multiple_static(
-	char_t (&querys)[N_BLOCKS][MAX_QUERY_LENGTH],
-	char_t (&references)[N_BLOCKS][MAX_REFERENCE_LENGTH],
-	idx_t (&query_lengths)[N_BLOCKS],
-	idx_t (&reference_lengths)[N_BLOCKS],
-	Penalties (&penalties)[N_BLOCKS],
+		char_t (&querys)[N_BLOCKS][MAX_QUERY_LENGTH],
+		char_t (&references)[N_BLOCKS][MAX_REFERENCE_LENGTH],
+		idx_t (&query_lengths)[N_BLOCKS],
+		idx_t (&reference_lengths)[N_BLOCKS],
+		Penalties (&penalties)[N_BLOCKS],
 #ifdef CMAKEDEBUG
-	tbr_t (&tb_streams)[N_BLOCKS][MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH],
-	Container (&debugger)[N_BLOCKS])
+		tbr_t (&tb_streams)[N_BLOCKS][MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH],
+		Container (&debugger)[N_BLOCKS])
 #else
-	tbr_t (&tb_streams)[N_BLOCKS][MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH])
+		tbr_t (&tb_streams)[N_BLOCKS][MAX_REFERENCE_LENGTH + MAX_QUERY_LENGTH])
 #endif
 	{
-// #pragma HLS interface mode = axis port = querys
-// #pragma HLS interface mode = axis port = references
-// #pragma HLS interface mode = axis port = query_lengths
-// #pragma HLS interface mode = axis port = reference_lengths
-// #pragma HLS interface mode = axis port = penalties
-// #pragma HLS interface mode = axis port = tb_streams
+		// AXIS Interface is not allowed in F1
+		// #pragma HLS interface mode = axis port = querys
+		// #pragma HLS interface mode = axis port = references
+		// #pragma HLS interface mode = axis port = query_lengths
+		// #pragma HLS interface mode = axis port = reference_lengths
+		// #pragma HLS interface mode = axis port = penalties
+		// #pragma HLS interface mode = axis port = tb_streams
 
+#pragma HLS array_partition variable = querys dim = 1 type = complete
+#pragma HLS array_partition variable = references dim = 1 type = complete
+#pragma HLS array_partition variable = query_lengths dim = 1 type = complete
+#pragma HLS array_partition variable = reference_lengths dim = 1 type = complete
+#pragma HLS array_partition variable = tb_streams dim = 1 type = complete
 
-#pragma HLS array_partition variable=querys dim=1 type=complete
-#pragma HLS array_partition variable=references dim=1 type=complete
-#pragma HLS array_partition variable=query_lengths dim=1 type=complete
-#pragma HLS array_partition variable=reference_lengths dim=1 type=complete
-#pragma HLS array_partition variable=tb_streams dim=1 type=complete
-
-		for (int i = 0; i < N_BLOCKS; i++){
+		for (int i = 0; i < N_BLOCKS; i++)
+		{
 #pragma HLS unroll
 			Align::AlignStatic(
 				querys[i],
@@ -63,17 +60,20 @@ extern "C"
 				penalties[i],
 				tb_streams[i]
 #ifdef CMAKEDEBUG
-				, debugger[i]
+				,
+				debugger[i]
 #endif
 			);
 
-			// Print For Demo Use
+#ifdef CMAKEDEBUG
+			// Print The Traceback Pointer Array
 			printf("Traceback Block: %d\n", i);
-			for (int j = 0; j < query_lengths[i] + reference_lengths[i]; j++){
-				
+			for (int j = 0; j < query_lengths[i] + reference_lengths[i]; j++)
+			{
 				printf("%d ", tb_streams[i][j].to_int());
 			}
-			printf("\n");  
+			printf("\n");
 		}
-    }
+#endif
+	}
 }
