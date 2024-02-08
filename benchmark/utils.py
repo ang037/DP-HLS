429737aeb5e2e16fa0b19fb9eca657eb422eee92
Yingqi Cao <yic033@ucsd.edu>
2024-02-08 00:36:03 +0000

    updated, fixed cmake
    Former-commit-id: a80b6268dcb2dc358f65747b1bfeb88c648c296d [formerly 04265c242b2d54e6c55ba172647029d122dce4fa]
    Former-commit-id: 756b7dccb1d654b8a397a97f49987ab099684dc0

--- benchmark/utils.py
+++ benchmark/utils.py
@@ -1,4 +1,4 @@
-import json
+import json, time
 import os, glob
 
 def extract_single_config_afi_id(base_address):
@@ -29,12 +29,45 @@ def extrace_all_configs_afi_id(base_address, config_names):
         afi_id_table[config_name] = extract_single_config_afi_id(os.path.join(base_address, config_name))
     return afi_id_table
 
-# write a simple test to test the function extract_single_config_afi_id
-print(extract_single_config_afi_id('/home/centos/workspace/kernels/global_affine/global_affine_256_256_16_16'))
+def run_benchmark_single(base_folder, host_name='dp-hls-host', xclbin_name='afi_creation.awsxclbin', repeat=10):
+    # check the repeat is larger than 2, if not, throw exception
+    if repeat < 2:
+        raise ValueError("Kernel Should be Replayed At Least 2 Times")
+    
+    # create an output folder in the base folder, suffix with its time
+    output_folder = os.path.join(base_folder, f'output_{time.time()}')
+    os.mkdir(output_folder)
 
-# write a simple test to test the function extrace_all_configs_afi_id
-list_of_configs = ['global_affine_256_256_16_8', 'global_affine_256_256_16_16', 'global_affine_256_256_16_32',
-                    'global_affine_256_256_32_8', 'global_affine_256_256_32_16', 'global_affine_256_256_32_32',
-                    'global_affine_256_256_64_8', 'global_affine_256_256_64_16', 'global_affine_256_256_64_32']
+    # the benchmark for single kernel have multiple runs. We want to create a output text file for ecah run
+    for i in range(repeat):
+        # create an output file for each run
+        output_file = os.path.join(output_folder, f'output_{i}.txt')
 
-print(extrace_all_configs_afi_id('/home/centos/workspace/kernels/global_affine', list_of_configs))
+        host_path = os.path.join(base_folder, host_name)
+        xclbin_path = os.path.join(base_folder, xclbin_name)
+
+        # run the benchmark with bash
+        os.system(f'bash {host_path} {xclbin_path} > {output_file}')
+
+    # then for all those files, it has a field of the format 'Kernel execution time: <num>ms'
+    # we want to discard the first two runs and take the average of the rest
+
+    # read all the output files and extract the kernel execution time
+    # we need to explicitly search for the line starting with 'Kernel execution time'
+    kernel_execution_time = []
+    for i in range(repeat):
+        output_file = os.path.join(output_folder, f'output_{i}.txt')
+        with open(output_file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.startswith('Kernel execution time'):
+                    kernel_execution_time.append(int(line.split(' ')[-1][-2]))
+
+    return sum(kernel_execution_time[2:])/(repeat-2)
+
+def run_benchmark_all():
+    pass
+
+
+# small test for the function benchmark_single
+print(run_benchmark_single('/home/centos/efs/kernels/global_affine/global_affine_256_256_16_8', 'dp-hls-host', 'afi_creation.awsxclbin', repeat=10))
