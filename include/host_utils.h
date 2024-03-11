f9375fa1580da3c15cb2f802a15e1b57ecca7fe9
Yingqi Cao <yic033@ucsd.edu>
2024-03-11 04:14:43 +0000

    II Violation Resolved
    Former-commit-id: 4f1928a48efd6a16cfd7ef5417aed1c6e8eeed55 [formerly fec4d32912bc99ebe872472e9edef542f193d84d]
    Former-commit-id: 3a7aa602e7803c434ffb189a2c044ca8a2d7db27

--- include/host_utils.h
+++ include/host_utils.h
@@ -18,8 +18,6 @@
 #include <complex>
 #include <random>
 #include <map>
-#include "xcl2.hpp"
-#include <CL/cl2.hpp>
 
 #include "params.h"
 
@@ -148,31 +146,6 @@ std::vector<string> ReconstructTracebackProfile(std::vector<string> querys, std:
 
 namespace HostUtils
 {
-    namespace CL
-    {
-        class KernelOperator {
-
-        };
-
-        class KernelWrapper {
-            // Need functions to
-            // 1. Create OpenCL Host Buffers and copy datas from the host memory to the buffer
-            // 2. Migrate the data from the cl buffer to device
-            // 3. Write the kernel bitstream to the first available device
-            // 4. Launch the kernel
-            // 5. Retrive the result. 
-            public:
-                cl_int err;
-                cl::Context context;
-                std::map<string, cl::Kernel> kernels;
-                std::map<string, cl::CommandQueue> queues;
-
-
-                // KernelWrapper();
-                // ~KernelWrapper();
-        };
-    }
-
     namespace IO
     {
         /**
