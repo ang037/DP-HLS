d8d7b388cf151ab29056611d3c58f730f62f696c
Yingqi Cao <yic033@ucsd.edu>
2024-04-05 08:03:48 +0000

    merge copy initial scores and query copying
    Former-commit-id: 53729b46f885dd6b4c8ff03b3f221c511d7c5c5b [formerly 2b8394ae329242bea86777978940d2b85e4e2570] [formerly e635bcafc224a468afcfb44728fe9605d0a4e7ce [formerly 8ac548c08cfd7e90eedad80f8a297940b809e785]]
    Former-commit-id: e635bcafc224a468afcfb44728fe9605d0a4e7ce
    Former-commit-id: 3c3e9051495bb899bdf1790131edf4e31ab0ca78

--- testbench/test_csim_global_linear.cpp
+++ testbench/test_csim_global_linear.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-#define INPUT_QUERY_LENGTH 50
-#define INPUT_REFERENCE_LENGTH 60
+#define INPUT_QUERY_LENGTH 94
+#define INPUT_REFERENCE_LENGTH 250
 
 char_t base_to_num(char base)
 {
