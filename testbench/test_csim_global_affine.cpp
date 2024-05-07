d04e87b6fc6cedbde5bd7c0269a9fa4493e3d714
Yingqi Cao <yic033@ucsd.edu>
2024-05-07 22:04:32 +0000

    Updated global and loca testbench file for benchmarking.
    Former-commit-id: 0371b813b0d26f663a6d39649b1ae7829d74c1d5

--- testbench/test_csim_global_affine.cpp
+++ testbench/test_csim_global_affine.cpp
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-#define INPUT_QUERY_LENGTH 35
-#define INPUT_REFERENCE_LENGTH 64
+#define INPUT_QUERY_LENGTH 256
+#define INPUT_REFERENCE_LENGTH 256
 
 char_t base_to_num(char base)
 {
