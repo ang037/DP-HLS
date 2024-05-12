bd4eda324be9e7544ac735035adcadf323f89319
Cloud User <centos@ip-172-31-20-114.ec2.internal>
2024-05-12 04:59:08 +0000

    Viterbi algorithm is fixed. The scores and the traceback matches my own solution.
    Former-commit-id: d8cc4913d48dfcb00149cfd80b234326b47c5625
    Former-commit-id: 34b460879152959070405c53df2d8146f6c88aa2

--- kernels/viterbi/kernel_viterbi.cpp
+++ kernels/viterbi/kernel_viterbi.cpp
@@ -17,16 +17,16 @@ void Viterbi::PE::Compute(char_t local_query_val,
      * Layer 2: Vj
      */
 
-    const type_t I_open = up_prev[1] + penalties.log_lambda; // Insert open
-    const type_t I_extend = up_prev[0] + penalties.log_1_m_mu;                  // insert extend
-    const type_t J_open = left_prev[1] + penalties.log_lambda;   // delete open
-    const type_t J_extend = left_prev[2] + penalties.log_1_m_mu;                    // delete extend
+    const type_t I_open = left_prev[1] + penalties.log_lambda; // Insert open
+    const type_t I_extend = left_prev[0] + penalties.log_1_m_mu;                  // insert extend
+    const type_t D_open = up_prev[1] + penalties.log_lambda;   // delete open
+    const type_t D_extend = up_prev[2] + penalties.log_1_m_mu;                    // delete extend
 
 #ifdef CMAKEDEBUG
     auto insert_open_s = I_open.to_float();     // Insert open
     auto insert_extend_s = I_extend.to_float(); // insert extend
-    auto delete_open_s = J_open.to_float();
-    auto delete_extend_s = J_extend.to_float();
+    auto delete_open_s = D_open.to_float();
+    auto delete_extend_s = D_extend.to_float();
 
     auto left_prev_0_s = left_prev[0].to_float();
     auto left_prev_1_s = left_prev[1].to_float();
@@ -37,11 +37,11 @@ void Viterbi::PE::Compute(char_t local_query_val,
 #endif
 
     bool I_open_b = I_open > I_extend;
-    bool J_open_b = J_open > J_extend;
-    write_score[0] = penalties.transition[ (int) local_query_val][4] + (I_open_b ? I_open : I_extend);
-    write_score[2] = penalties.transition[4][(int) local_reference_val] + (J_open_b ? J_open : J_extend);
+    bool D_open_b = D_open > D_extend;
+    write_score[0] = penalties.transition[local_query_val][4] + (I_open_b ? I_open : I_extend);
+    write_score[2] = penalties.transition[4][local_reference_val] + (D_open_b ? D_open : D_extend);
     tbp_t insert_tb = I_open_b ? (tbp_t) 0 : TB_IMAT;
-    tbp_t delete_tb = J_open_b ? (tbp_t) 0 : TB_JMAT;
+    tbp_t delete_tb = D_open_b ? (tbp_t) 0 : TB_JMAT;
 
 #ifdef CMAKEDEBUG
     auto write_score_0_s = write_score[0].to_float();
@@ -49,8 +49,8 @@ void Viterbi::PE::Compute(char_t local_query_val,
 #endif
 
     const type_t match_M = penalties.log_1_m_2_lambda + diag_prev[1];
-    const type_t match_I = penalties.log_mu + diag_prev[0];
-    const type_t match_J = penalties.log_mu + diag_prev[2];
+    const type_t match_I = penalties.log_mu + left_prev[0];
+    const type_t match_D = penalties.log_mu + up_prev[2];
 
 #ifdef CMAKEDEBUG
     auto diag_prev_s = diag_prev[1].to_float();
@@ -60,6 +60,8 @@ void Viterbi::PE::Compute(char_t local_query_val,
 
     type_t max_val = match_M;
     tbp_t dir_tb = TB_DIAG;
+
+    type_t transition_score = penalties.transition[local_query_val][local_reference_val];
 
 #ifdef CMAKEDEBUG
     auto write_score_1_s = write_score[1].to_float();
@@ -72,15 +74,15 @@ void Viterbi::PE::Compute(char_t local_query_val,
         dir_tb = TB_LEFT;
     }
 
-    if (max_val < match_J)
+    if (max_val < match_D)
     {
-        max_val = match_J;
+        max_val = match_D;
         dir_tb = TB_UP;
     }
 
-    write_score[1] = penalties.transition[local_query_val][local_reference_val] + max_val;
+    write_score[1] = transition_score + max_val;
 
-    write_traceback = dir_tb + insert_tb + delete_tb;
+    write_traceback = dir_tb | insert_tb | delete_tb;
 }
 
 void Viterbi::InitializeScores(
@@ -143,6 +145,8 @@ void Viterbi::Traceback::StateMapping(tbp_t tbp, TB_STATE &state, tbr_t &navigat
         {
             state = TB_STATE::INS;
             navigation = AL_NULL;
+        } else {
+            navigation = AL_END;
         }
     }
     else if (state == TB_STATE::DEL)
