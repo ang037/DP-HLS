0153bd352decc3ef491da0450ea387090c595b4e
Cloud User <centos@ip-172-31-20-114.ec2.internal>
2024-05-12 04:59:08 +0000

    Viterbi algorithm is fixed. The scores and the traceback matches my own solution.
    Former-commit-id: 34b460879152959070405c53df2d8146f6c88aa2 [formerly d8cc4913d48dfcb00149cfd80b234326b47c5625]
    Former-commit-id: 5f7dfec20e88e2ea7d8e8f9ee9d6697694d20908

--- include/solution_viterbi.h
+++ include/solution_viterbi.h
@@ -11,26 +11,27 @@
 #include <complex>
 #include <set>
 #include "host_utils.h"
+#include <cassert>
 
 /**
- * @brief Viterbi itself is a global alignment, don't need to consider the case of making it local. 
- * 
- * @tparam PENALTY_T 
- * @tparam SOL_MAX_QUERY_LENGTH 
- * @tparam SOL_MAX_REFERENCE_LENGTH 
- * @tparam SOL_N_LAYERS 
- * @param query 
- * @param reference 
- * @param penalties 
- * @param score_mat 
- * @param tb_mat 
- * @param alignments 
+ * @brief Viterbi itself is a global alignment, don't need to consider the case of making it local.
+ *
+ * @tparam PENALTY_T
+ * @tparam SOL_MAX_QUERY_LENGTH
+ * @tparam SOL_MAX_REFERENCE_LENGTH
+ * @tparam SOL_N_LAYERS
+ * @param query
+ * @param reference
+ * @param penalties
+ * @param score_mat
+ * @param tb_mat
+ * @param alignments
  */
 template <typename PENALTY_T, int SOL_MAX_QUERY_LENGTH, int SOL_MAX_REFERENCE_LENGTH, int SOL_N_LAYERS>
 void viterbi_solution(std::string query, std::string reference, PENALTY_T &penalties,
-                            array<array<array<double, SOL_MAX_REFERENCE_LENGTH>, SOL_MAX_QUERY_LENGTH>, SOL_N_LAYERS> &score_mat,
-                            array<array<string, SOL_MAX_REFERENCE_LENGTH>, SOL_MAX_QUERY_LENGTH> &tb_mat,
-                            map<string, string> &alignments)
+                      array<array<array<double, SOL_MAX_REFERENCE_LENGTH>, SOL_MAX_QUERY_LENGTH>, SOL_N_LAYERS> &score_mat,
+                      array<array<string, SOL_MAX_REFERENCE_LENGTH>, SOL_MAX_QUERY_LENGTH> &tb_mat,
+                      map<string, string> &alignments)
 {
     // Layer 0: Insertion Matrix
     // Layer 1: Match Mismatch Matrix
@@ -45,17 +46,17 @@ void viterbi_solution(std::string query, std::string reference, PENALTY_T &penal
     for (int i = 0; i < SOL_MAX_QUERY_LENGTH; i++)
     {
         init_penal += penalties.log_1_m_mu;
-        initial_col[i][0] = -numeric_limits<double>::infinity();
+        initial_col[i][0] = -1000000;
         initial_col[i][1] = init_penal;
-        initial_col[i][2] = -numeric_limits<double>::infinity(); // This can be whatever, since won't be accessed
+        initial_col[i][2] = -1000000; // This can be whatever, since won't be accessed
     }
     init_penal = penalties.log_lambda;
     for (int j = 0; j < SOL_MAX_REFERENCE_LENGTH; j++)
     {
         init_penal += penalties.log_1_m_mu;
-        initial_row[j][0] = -numeric_limits<double>::infinity(); // This can be whatever, since won't be accessed
+        initial_row[j][0] = -1000000; // This can be whatever, since won't be accessed
         initial_row[j][1] = init_penal;
-        initial_row[j][2] = -numeric_limits<double>::infinity();
+        initial_row[j][2] = -1000000;
     }
 
     // Initialize the score matrix
@@ -82,34 +83,36 @@ void viterbi_solution(std::string query, std::string reference, PENALTY_T &penal
     // Fill in the DP matrix and traceback matrix
     for (int i = 0; i < query.length(); i++)
     {
-        double del_up, del_diag;
-        double ins_left, ins_diag;
-        double main_diag, main_up, main_left;
+
         for (int j = 0; j < reference.length(); j++)
         {
+            double del_up;
+            double ins_left;
+            double main_diag, main_up, main_left;
+
             if (i == 0 && j == 0)
             {
-                main_diag = 0;
-                main_up = initial_row[j][1];
-                main_left = initial_col[i][1];
+                main_diag = 0.0;
+                main_up = initial_row[0][1];
+                main_left = initial_col[0][1];
 
-                ins_left = initial_col[i][0];
-                ins_diag = 0;
+                ins_left = initial_col[0][0];
+                // ins_diag = 0;
 
-                del_up = initial_row[j][2];
-                del_diag = 0;
+                del_up = initial_row[0][2];
+                // del_diag = 0;
             }
-            else if (i == 0 && j > 0)
+            else if (i == 0 && j > 0)  // In first row, not column
             {
                 main_diag = initial_row[j - 1][1];
                 main_up = initial_row[j][1];
                 main_left = score_mat[1][i][j - 1];
 
-                ins_left = score_mat[0][i][j - 1];
-                ins_diag = initial_row[j - 1][0];
+                ins_left = score_mat[0][0][j - 1];
+                // ins_diag = initial_row[j - 1][0];
 
                 del_up = initial_row[j][2];
-                del_diag = initial_row[j - 1][2];
+                // del_diag = initial_row[j - 1][2];
             }
             else if (i > 0 && j == 0)
             {
@@ -118,10 +121,10 @@ void viterbi_solution(std::string query, std::string reference, PENALTY_T &penal
                 main_left = initial_col[i][1];
 
                 ins_left = initial_col[i][0];
-                ins_diag = initial_col[i - 1][0];
+                // ins_diag = initial_col[i - 1][0];
 
                 del_up = score_mat[2][i - 1][j];
-                del_diag = initial_col[i - 1][2];
+                // del_diag = initial_col[i - 1][2];
             }
             else
             {
@@ -130,36 +133,32 @@ void viterbi_solution(std::string query, std::string reference, PENALTY_T &penal
                 main_left = score_mat[1][i][j - 1];
 
                 ins_left = score_mat[0][i][j - 1];
-                ins_diag = score_mat[0][i-1][j-1];
+                // ins_diag = score_mat[0][i-1][j-1];
 
                 del_up = score_mat[2][i - 1][j];
-                del_diag = score_mat[2][i-1][j-1];
+                // del_diag = score_mat[2][i-1][j-1];
             }
 
-            double del_write, ins_write, main_write;
-            double ins_open, ins_extend, del_open, del_extend;
+            double del_write, ins_write, main_write;           // values write to the score matrix
+            double ins_open, ins_extend, del_open, del_extend; // values for the insertion and deletion matrix
             ins_open = main_left + penalties.log_lambda;
             ins_extend = ins_left + penalties.log_1_m_mu;
             del_open = main_up + penalties.log_lambda;
             del_extend = del_up + penalties.log_1_m_mu;
-            
+
             bool ins_open_b = ins_extend < ins_open;
             bool del_open_b = del_extend < del_open;
-            ins_write = penalties.transition[4][HostUtils::Sequence::base_to_num(reference[j])] + (ins_open_b ? ins_open : ins_extend);
-            del_write = penalties.transition[HostUtils::Sequence::base_to_num(query[i])][4] + (del_open_b ? del_open : del_extend);
+            ins_write = penalties.transition[HostUtils::Sequence::base_to_num(query[i])][4] + (ins_open_b ? ins_open : ins_extend);
+            del_write = penalties.transition[4][HostUtils::Sequence::base_to_num(reference[j])] + (del_open_b ? del_open : del_extend);
 
             double main_match = penalties.log_1_m_2_lambda + main_diag;
-            double main_ins = penalties.log_mu + ins_diag;
-            double main_del = penalties.log_mu + del_diag;
+            double main_ins = penalties.log_mu + ins_left;
+            double main_del = penalties.log_mu + del_up;
 
             double main_max = main_match;
             main_max = main_max > main_ins ? main_max : main_ins;
             main_max = main_max > main_del ? main_max : main_del;
-            main_write = penalties.transition[
-                HostUtils::Sequence::base_to_num(query[i])
-            ][
-                HostUtils::Sequence::base_to_num(reference[j])
-            ] + main_max;
+            main_write = penalties.transition[HostUtils::Sequence::base_to_num(query[i])][HostUtils::Sequence::base_to_num(reference[j])] + main_max;
 
             score_mat[0][i][j] = ins_write;
             score_mat[1][i][j] = main_write;
@@ -174,9 +173,13 @@ void viterbi_solution(std::string query, std::string reference, PENALTY_T &penal
             {
                 tb_mat[i][j] = del_open_b ? "U " : "UE"; // 'U' indicates an up direction (deletion)
             }
-            else
+            else if (main_max == main_match)
             {
                 tb_mat[i][j] = "D "; // 'D' indicates a diagonal direction (match or mismatch)
+            }
+            else
+            {
+                cout << "ERROR: Invalid traceback matrix value" << endl;
             }
         }
     }
