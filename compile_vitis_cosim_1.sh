7f6035d42fa189732c4eea1c09e489f762a59bc5
Yingqi Cao <yic033@ucsd.edu>
2024-05-14 08:51:43 +0000

    minor updates to testbench length
    Former-commit-id: 6a59968688eaf561c11caab77ca67a3417f00258
    Former-commit-id: b4a5486b88ab49930363c94301b7ac0cc7cba820

--- compile_vitis_cosim_1.sh
+++ compile_vitis_cosim_1.sh
@@ -7,4 +7,4 @@
 # python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/global_two_piece_affine/two_piece_affine_common_config.json --simulate True
 # python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/global_dtw/global_dtw_common_configs.json --simulate True
 # python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/local_linear/local_linear_common_configs.json --simulate True
-python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/compile_configs/viterbi/viterbi_common_configs.json --simulate True
+python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/semiglobal_short_long/local_linear_common_configs.json --simulate True
