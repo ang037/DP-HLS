caec92d7d348fcfd30a32911e077ac2013d99f52
Yingqi Cao <yic033@ucsd.edu>
2024-05-30 21:19:09 +0000

    Created configs for banding kernels, including banding global linear, banding two piece affine, and banding local affine.
    Former-commit-id: 98ea266838a36662362102d9f4db164d169f2b39
    Former-commit-id: a86f1945ec6b5c034996713a1a7a70ed6d214794

---
+++ kernels/banding_local_affine/params.h
@@ -0,0 +1,64 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#include <ap_int.h>
+#include <ap_fixed.h>
+#include <hls_vector.h>
+
+#define MAX_QUERY_LENGTH 128
+#define MAX_REFERENCE_LENGTH 128
+#define N_BLOCKS 1
+#define PE_NUM 8
+
+#define BANDING FIXED
+#define BANDWIDTH 64
+
+#define ALIGN_TYPE BandingLocalAffine
+#define N_LAYERS 3
+#define LAYER_MAXIMIUM 1  // We need to indicate from which layer (main matrix) is the maximum score stored.
+
+
+// Primitive Types
+typedef ap_uint<2> char_t;  // Sequence Alphabet
+typedef ap_fixed<16, 12> type_t;  // Scores Type <width, integer_width>
+typedef short idx_t;  // Indexing Type, could be much less than 32. ap_uint<8>
+typedef ap_uint<4> tbp_t;  // Traceback Pointer Type
+
+// Define Zero Value
+#define zero_fp ((type_t)0)
+#define ZERO_CHAR (char_t(0))
+
+// Defien upper and lower bound for score type, aka type_t
+#define INF 1024
+#define NINF -1024
+
+// Legacy Debugger Configuration
+#define DEBUG_OUTPUT_PATH "/home/yic033@AD.UCSD.EDU/DP-HLS-Debug/local_affine/"
+#define DEBUG_FILENAME "debug_kernel"
+
+#define TB_PH (tbp_t) 0b00
+#define TB_LEFT (tbp_t) 0b01
+#define TB_DIAG (tbp_t) 0b10
+#define TB_UP (tbp_t) 0b11
+
+#define TB_IMAT (tbp_t) 0b0100  // Insertion Matrix
+#define TB_DMAT (tbp_t) 0b1000  // Deletion Matrix
+
+
+struct Penalties {
+    type_t open;
+    type_t extend;
+    type_t mismatch;
+    type_t match;
+    type_t linear_gap;
+};
+
+enum TB_STATE {
+    MM = 0,   // Match/Mismatch
+    INS = 1,  // Insertion
+    DEL = 2,  // Deletion
+    END = 3   // End
+};
+
+
+#endif
