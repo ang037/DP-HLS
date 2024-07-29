6cc531838af9e47fa51f0385a6576fdcac83a826
Yingqi Cao <yic033@ucsd.edu>
2024-07-29 07:29:45 +0000

    cleaned repository
    Former-commit-id: 83551602fa46b6c8c231e276a72833b71aec85c8 [formerly 0517927447fae887f1cb397f0ffa70010d2e5232]
    Former-commit-id: 42b573dfe93b01cc488e0e7954f83c89defee279

--- cosim_scaling.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/scaling_global_linear/global_linear_scaling_pe.json --simulate True
-# python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/scaling_global_linear/global_linear_scaling_blocks.json --simulate True
-# python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/scaling_global_dtw/global_dtw_scaling_pes.json --simulate True
-python /home/centos/workspace/DP-HLS/py-hls/auto_cosim.py --config /home/centos/workspace/DP-HLS/compile_configs/scaling_global_dtw/global_dtw_scaling_blocks.json --simulate True
