d1fbed99f10b43724864161581ef7693dca9dc46
Yingqi Cao <yic033@ucsd.edu>
2024-07-29 07:29:45 +0000

    cleaned repository
    Former-commit-id: 90c94514ba9038f905282763fdf4c67c584e7db4 [formerly 83551602fa46b6c8c231e276a72833b71aec85c8] [formerly 0517927447fae887f1cb397f0ffa70010d2e5232]
    Former-commit-id: 42b573dfe93b01cc488e0e7954f83c89defee279
    Former-commit-id: 6cc531838af9e47fa51f0385a6576fdcac83a826

--- further_throughput_1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python /home/centos/workspace/DP-HLS/py-hls/parallel_compile.py --config /home/centos/workspace/DP-HLS/compile_configs/global_two_piece_affine/two_piece_affine_common_config_max.json --compile True --num_workers 1 --all True
-python /home/centos/workspace/DP-HLS/py-hls/parallel_compile.py --config /home/centos/workspace/DP-HLS/compile_configs/global_affine/global_affine_common_config.json --compile True --num_workers 1 --all True
-python /home/centos/workspace/DP-HLS/py-hls/parallel_compile.py --config /home/centos/workspace/DP-HLS/compile_configs/local_linear/local_linear_common_configs.json --compile True --num_workers 1 --all True
--- reinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python setup.py clean --all
-pip install -v -e .
-python demo.py
