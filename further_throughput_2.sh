83551602fa46b6c8c231e276a72833b71aec85c8
Yingqi Cao <yic033@ucsd.edu>
2024-07-29 07:29:45 +0000

    cleaned repository
    Former-commit-id: 0517927447fae887f1cb397f0ffa70010d2e5232

--- further_throughput_2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python /home/centos/workspace/DP-HLS/py-hls/parallel_compile.py --config /home/centos/workspace/DP-HLS/compile_configs/overlap_suffix_prefix/overlap_suffix_prefix_config.json --compile True --num_workers 1 --all True
-python /home/centos/workspace/DP-HLS/py-hls/parallel_compile.py --config /home/centos/workspace/DP-HLS/compile_configs/semiglobal_short_long/local_linear_common_configs.json --compile True --num_workers 1 --all True
