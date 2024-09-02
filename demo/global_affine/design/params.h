783cc9afe0b9a6a176e8cb23f5d99a75e099624a
Yingqi Cao <yic033@ucsd.edu>
2024-09-02 01:25:13 -0700

    completed the demo and sample files.
    Former-commit-id: 4c1c20d9ac7d669079692bfdb6f1e1c668b50575

---
+++ demo/global_affine/design/params.h
@@ -0,0 +1,56 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#include <ap_int.h>
+#include <ap_fixed.h>
+#include <hls_vector.h>
+
+
+// These need to be there to do CMake Simulation, but SHOULD TAKE OFF WHEN COMPIING BITSTREAM
+const int PE_NUM = 32;
+#define MAX_QUERY_LENGTH 256
+#define MAX_REFERENCE_LENGTH 256
+#define N_BLOCKS 1
+
+#define ALIGN_TYPE GlobalAffine
+#define N_LAYERS 3
+#define LAYER_MAXIMIUM 1  // We need to indicate from which layer (main matrix) is the maximum score stored.
+
+// if user decides to use banding
+#define BANDING RECTANGULAR
+
+// Primitive Types
+typedef ap_uint<2> char_t;  // Sequence Alphabet
+typedef ap_fixed<16, 14> type_t;  // Scores Type <width, integer_width>
+typedef short idx_t;  // Indexing Type, could be much less than 32. ap_uint<8>
+typedef ap_uint<4> tbp_t;  // Traceback Pointer Type
+
+
+// Defien upper and lower bound for score type, aka type_t
+#define INF 1024
+#define NINF -1024
+
+// Legacy Debugger Configuration
+#define DEBUG_OUTPUT_PATH "/home/yic033@AD.UCSD.EDU/DP-HLS-Debug/global_affine/"
+#define DEBUG_FILENAME "debug_kernel"
+
+
+struct Penalties {
+    type_t open;
+    type_t extend;
+    type_t mismatch;
+    type_t match;
+    type_t linear_gap;
+};
+
+enum TB_STATE {
+    MM = 0,   // Match/Mismatch
+    INS = 1,  // Insertion
+    DEL = 2,  // Deletion
+    END = 3   // End
+};
+
+#define ZERO_CHAR (char_t(0))
+#define zero_fp ((type_t)0)
+
+#endif
