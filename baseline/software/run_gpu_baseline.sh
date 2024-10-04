f84247bb01ad72c3126f5d6be4466098234de372
jaysunl <jsliang@ucsd.edu>
2024-10-03 17:58:30 -0700

    fix file not found error

--- baseline/software/run_gpu_baseline.sh
+++ baseline/software/run_gpu_baseline.sh
@@ -20,13 +20,13 @@ make GPU_SM_ARCH=$ARCH_CODE MAX_QUERY_LEN=256 N_CODE=0x4E    # E.g. GPU_SM_ARCH=
 cd test_prog
 make clean
 make -j
-
+pwd
 echo
 # Local  
-./test_prog.out -y local ../data/pbsim2/ont_query.fa ../data/pbsim2/ont_ref.fa
+./test_prog.out -y local ../../data/pbsim2/ont_query.fa ../../data/pbsim2/ont_ref.fa
 
 # Global
-./test_prog.out -y global ../data/pbsim2/ont_query.fa ../data/pbsim2/ont_ref.fa
+./test_prog.out -y global ../../data/pbsim2/ont_query.fa ../../data/pbsim2/ont_ref.fa
 
 # Banded
-./test_prog.out -k 64 -y local ../data/pbsim2/ont_query.fa ../data/pbsim2/ont_ref.fa
+./test_prog.out -k 64 -y local ../../data/pbsim2/ont_query.fa ../../data/pbsim2/ont_ref.fa
